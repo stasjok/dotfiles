37e091d1ea3e59a18d0125ce0107d95a981f5017
Stanislav Asunkin <1353637+stasjok@users.noreply.github.com>
2021-07-06 20:05:27 +0300

    feat(packages): add sample nix expression (git installation)

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
-# dotfiles
-My dev configuration.
+# My dotfiles
+
+## Packages
+
+Install Nix (https://nixos.org/manual/nix/stable/#chap-installation), then:
+
+```
+nix-env --install --remove-all --file packages.nix
+```
---
+++ packages.nix
@@ -0,0 +1,18 @@
+let
+  nixpkgs-stable = fetchTarball {
+    name = "nixos-21.05.1268.21b696caf39";
+    url = https://releases.nixos.org/nixos/21.05/nixos-21.05.1268.21b696caf39/nixexprs.tar.xz;
+    sha256 = "1ffk57wfcfvvfjcxzialn9pgfmkanygz4h9kswv0jwiypjbrhzaa";
+  };
+  nixpkgs-unstable = fetchTarball {
+    name = "nixpkgs-21.11pre300283.f930ea227ce";
+    url = https://releases.nixos.org/nixpkgs/nixpkgs-21.11pre300283.f930ea227ce/nixexprs.tar.xz;
+    sha256 = "1jw4s2psbx2ib1w69nymxj4jcyqgjg19n818ygc1rlspjscjfgyg";
+  };
+
+  stable = import nixpkgs-stable { config = {}; overlays = []; };
+  unstable = import nixpkgs-unstable { config = {}; overlays = []; };
+
+in with stable; [
+  git
+]
