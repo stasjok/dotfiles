4041b5796b0e00e42c38b702670a2317f372e7f4
Stanislav Asunkin <1353637+stasjok@users.noreply.github.com>
2021-07-14 15:10:41 +0300

    refactor(nvim): lazy-load plugins

--- nvim/lua/my/plugins.lua
+++ nvim/lua/my/plugins.lua
@@ -14,11 +14,34 @@ packer.startup({
     { 'nanotee/nvim-lua-guide', commit = 'bcbc72d9c0952d30d603e48603da034b0f095e61' },
 
     -- Tree-sitter
-    { 'nvim-treesitter/nvim-treesitter', commit = '29113e6892a46d4afff41417c0be7122a3b97ae6',
-    run = ':TSUpdate', config = require'my.treesitter'.setup },
+    {
+      'nvim-treesitter/nvim-treesitter', commit = '29113e6892a46d4afff41417c0be7122a3b97ae6',
+      run = ':TSUpdate', config = require'my.treesitter'.setup,
+      ft = {
+        'nix',
+      },
+      cmd = {
+        'TSInstall',
+        'TSInstallFromGrammar',
+        'TSInstallInfo',
+        'TSUpdate',
+        'TSUninstall',
+        'TSConfigInfo',
+        'TSBufEnable',
+        'TSBufDisable',
+        'TSBufToggle',
+        'TSEnableAll',
+        'TSDisableAll',
+        'TSToggleAll',
+        'TSModuleInfo',
+        'TSEditQuery',
+        'TSEditQueryUserAfter'
+      },
+      module = 'nvim-treesitter.ts_utils'
+    },
 
     -- Nix
-    { 'Freed-Wu/vim-nix', commit = '2fc254b90661f8190565b18874d0662bfcbec02c' }, -- forked from LnL7/vim-nix
+    { 'Freed-Wu/vim-nix', commit = '2fc254b90661f8190565b18874d0662bfcbec02c', ft = 'nix' }, -- forked from LnL7/vim-nix
   }
 });
 
