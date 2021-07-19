9291a2b5fe2519a3372864c207f9fc5d17d1985f
Stanislav Asunkin <1353637+stasjok@users.noreply.github.com>
2021-07-19 20:03:26 +0300

    feat(nvim): switch buffers on '<space><cr>', clear hlsearch on enter

--- nvim/init.lua
+++ nvim/init.lua
@@ -6,7 +6,23 @@ vim.api.nvim_command('colorscheme solarized');
 -- Command for installing plugins
 vim.cmd [[command! Plugins runtime lua/my/plugins.lua]]
 
+-- Set <leader> to Space and <localleader> to \
+vim.g.mapleader = ' ';
+vim.g.maplocalleader = '\\';
+
 -- Options
 vim.opt.hidden = true;
 vim.opt.ignorecase = true;
 vim.opt.smartcase = true;
+vim.opt.ttimeoutlen = 5;
+
+-- Mappings
+vim.api.nvim_set_keymap('n', '<leader><CR>', '<Cmd>buffer #<CR>', {noremap = true});
+function _G.OnEnter()
+  if vim.v.hlsearch == 1 then
+    return vim.api.nvim_replace_termcodes('<Cmd>nohlsearch<CR>', true, true, true);
+  else
+    return vim.api.nvim_replace_termcodes('<CR>', true, true, true);
+  end
+end
+vim.api.nvim_set_keymap('n', '<CR>', 'v:lua.OnEnter()', {expr = true, noremap = true});
