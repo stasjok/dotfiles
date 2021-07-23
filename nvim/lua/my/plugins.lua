a9739115591c3074616afbc8bbae5c4049356b40
Stanislav Asunkin <1353637+stasjok@users.noreply.github.com>
2021-07-23 21:43:37 +0300

    fix(nvim): remove spaces only inside pairs

--- nvim/lua/my/plugins.lua
+++ nvim/lua/my/plugins.lua
@@ -71,12 +71,17 @@ packer.startup({
         -- But I've changed it a little
         npairs.add_rules {
           Rule(' ', ' ')
-            :with_pair(function (opts)
+            :with_pair(function(opts)
               local pair = opts.line:sub(opts.col, opts.col + 1)
-              return vim.tbl_contains({ '()', '[]', '{}' }, pair)
+              return vim.tbl_contains({ '()', '{}', '[]' }, pair)
             end)
             :with_move(cond.none())
-            :with_cr(cond.none()),
+            :with_cr(cond.none())
+            :with_del(function(opts)
+              local col = vim.api.nvim_win_get_cursor(0)[2]
+              local context = opts.line:sub(col - 1, col + 2)
+              return vim.tbl_contains({ '(  )', '{  }', '[  ]' }, context)
+            end),
           Rule('', ' )')
             :with_pair(cond.none())
             :with_move(function(opts) return opts.char == ')' end)
