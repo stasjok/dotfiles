8b49a8f526ee4bc6d1e8971678ad50b94ec9b06c
Stanislav Asunkin <1353637+stasjok@users.noreply.github.com>
2021-07-25 13:36:12 +0300

    refactor(nvim): refactor terminal bindings
    - use startinsert only after hotkey, not BufWinEnter
    - merge buffer options into one autocmd
    - use separate variable for storing bufnr (fix nil on plugin reloading)
    - check if buffer is valid before switching

--- nvim/plugin/terminal.lua
+++ nvim/plugin/terminal.lua
@@ -1,25 +1,25 @@
 vim.cmd([[
 augroup terminal
   autocmd!
-  autocmd TermOpen * setlocal nonumber norelativenumber sidescrolloff=0
-  autocmd TermOpen * startinsert
-  autocmd BufWinEnter term://* startinsert
+  autocmd TermOpen * setlocal nonumber norelativenumber sidescrolloff=0 | startinsert
 augroup END
 ]]);
 
 _G.Terminal = {
   Open = function()
-    if _G.Terminal.Buffer == nil then
-      _G.Terminal.Buffer = vim.api.nvim_create_buf(true, false)
-      vim.api.nvim_set_current_buf(_G.Terminal.Buffer)
+    if _G.TerminalBuffer == nil or not vim.api.nvim_buf_is_valid(_G.TerminalBuffer) then
+      _G.TerminalBuffer = vim.api.nvim_create_buf(true, false)
+      vim.api.nvim_set_current_buf(_G.TerminalBuffer)
       vim.fn.termopen('fish', {on_exit = _G.Terminal.OnExit})
+      vim.api.nvim_command('startinsert')
     else
-      vim.api.nvim_set_current_buf(_G.Terminal.Buffer)
+      vim.api.nvim_set_current_buf(_G.TerminalBuffer)
+      vim.api.nvim_command('startinsert')
     end
   end,
   OnExit = function()
-    vim.api.nvim_buf_delete(_G.Terminal.Buffer, {force = true})
-    _G.Terminal.Buffer = nil
+    vim.api.nvim_buf_delete(_G.TerminalBuffer, {force = true})
+    _G.TerminalBuffer = nil
   end,
 }
 
