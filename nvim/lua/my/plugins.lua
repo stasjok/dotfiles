8595f7e6e71c6bea0b71c5ffe2fc815d00e86436
Stanislav Asunkin <1353637+stasjok@users.noreply.github.com>
2021-08-02 20:48:37 +0300

    feat(nvim/lsp): add icons for lsp completion source

--- nvim/lua/my/plugins.lua
+++ nvim/lua/my/plugins.lua
@@ -287,6 +287,35 @@ packer.startup({
           augroup END
         ]]
 
+        -- Completion icons
+        local icons = {
+          Class = " ",
+          Color = " ",
+          Constant = " ",
+          Constructor = " ",
+          Enum = "了 ",
+          EnumMember = " ",
+          Field = " ",
+          File = " ",
+          Folder = " ",
+          Function = " ",
+          Interface = "ﰮ ",
+          Keyword = " ",
+          Method = "ƒ ",
+          Module = " ",
+          Property = " ",
+          Snippet = "﬌ ",
+          Struct = " ",
+          Text = " ",
+          Unit = " ",
+          Value = " ",
+          Variable = " ",
+        }
+        local kinds = vim.lsp.protocol.CompletionItemKind
+        for i, kind in ipairs(kinds) do
+          kinds[i] = icons[kind] or kind
+        end
+
         -- Lua language server
         local runtime_path = vim.split(package.path, ';')
         table.insert(runtime_path, "lua/?.lua")
