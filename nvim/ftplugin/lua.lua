c231b75d9c106d7d947ba9236dc966725fe65461
Stanislav Asunkin <1353637+stasjok@users.noreply.github.com>
2021-08-11 00:06:17 +0300

    feat(nvim/lua): format lua on save, but only if stylua.conf exists

--- nvim/ftplugin/lua.lua
+++ nvim/ftplugin/lua.lua
@@ -1,6 +1,23 @@
+local lspconfig = require("lspconfig")
+local lsputil = require("lspconfig.util")
+
 vim.opt.shiftwidth = 2
 
 -- Source current file or selected lines
 for _, m in ipairs({ "n", "x" }) do
   vim.api.nvim_buf_set_keymap(0, m, "<LocalLeader>s", ":source<CR>", { noremap = true })
+end
+
+-- Auto-format on save
+local root_dir = lspconfig["null-ls"].get_root_dir(vim.api.nvim_buf_get_name(0))
+local stylua_conf_exists = lsputil.path.exists(root_dir .. "/stylua.toml")
+  or lsputil.path.exists(root_dir .. "/.stylua.toml")
+
+if stylua_conf_exists then
+  vim.cmd([[
+    augroup LuaAutoFormatting
+    autocmd!
+    autocmd BufWritePre <buffer> lua vim.lsp.buf.formatting_sync(nil, 1000)
+    augroup END
+  ]])
 end
