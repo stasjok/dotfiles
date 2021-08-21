aa92852f225c907c1a5dcece7110a0398ab28ed0
Stanislav Asunkin <1353637+stasjok@users.noreply.github.com>
2021-08-21 13:04:18 +0300

    feat(nvim/plugins): display packer in a floating window

--- nvim/plugin/plugins.lua
+++ nvim/plugin/plugins.lua
@@ -8,6 +8,11 @@ packer.startup({
   config = {
     compile_path = join_paths(vim.fn.stdpath("data"), "site", "plugin", "packer_compiled.lua"),
     auto_reload_compiled = false,
+    display = {
+      open_fn = function()
+        return require("packer.util").float({ border = "rounded" })
+      end,
+    },
     profile = {
       enable = true,
     },
