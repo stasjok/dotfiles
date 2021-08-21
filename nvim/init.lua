55baf54d80df535a1ab1890ac8ce58b84576ee60
Stanislav Asunkin <1353637+stasjok@users.noreply.github.com>
2021-08-21 13:40:17 +0300

    feat(nvim): update _G.dump() function and rename it to _G.put()
    Updated to latest version from nvim-lua-guide. Now it:
    * can display nil values
    * splits arguments by newline

--- nvim/init.lua
+++ nvim/init.lua
@@ -159,8 +159,12 @@ augroup END
 ]])
 
 -- From nvim-lua-guide
-function _G.dump(...)
-  local objects = vim.tbl_map(vim.inspect, { ... })
-  print(unpack(objects))
-  return ...
+function _G.put(...)
+  local objects = {}
+  for i = 1, select("#", ...) do
+    local v = select(i, ...)
+    table.insert(objects, vim.inspect(v))
+  end
+
+  print(table.concat(objects, "\n"))
 end
