6bbaaea1a214ac60cc0d55b2cd3fe1742d03eff1
Stanislav Asunkin <1353637+stasjok@users.noreply.github.com>
2021-08-26 22:06:29 +0300

    fix(update): possibly fix updating plugins
    Right now packer doesn't fetch new commits before checking out, but it
    does fetch commits after. So before we pull new updates we'll sync plugins.

--- update.sh
+++ update.sh
@@ -8,6 +8,11 @@ cd "$(dirname "${BASH_SOURCE[0]}")"
 
 # Avoid infinite loop
 if [[ $1 != --no-pull ]]; then
+    # Right now packer doesn't fetch new commits before checking out, but it
+    # does fetch commits after. So before we pull new updates we sync plugins.
+    # https://github.com/wbthomason/packer.nvim/issues/558
+    nvim --headless -c "autocmd User PackerComplete quitall" -c "PackerSync"
+
     git pull
     exec ./"$script_name" --no-pull
 fi
