38104f73fb1295bff19fe0b732917fd2683fc575
Stanislav Asunkin <1353637+stasjok@users.noreply.github.com>
2021-09-01 22:28:50 +0300

    refactor(nvim/lspconfig): configure language servers in loop

--- nvim/lua/plugins/lspconfig.lua
+++ nvim/lua/plugins/lspconfig.lua
@@ -28,111 +28,92 @@ function lspconfig.config()
     vim.fn.sign_define(hl, { text = icon, texthl = hl, numhl = "" })
   end
 
-  local luadev = require("lua-dev").setup({
-    lspconfig = {
-      cmd = { "lua-language-server" },
-      on_attach = utils.on_attach,
-      capabilities = capabilities,
-      flags = {
-        debounce_text_changes = 100,
-      },
-    },
-  })
-  require("lspconfig").sumneko_lua.setup(luadev)
-
-  require("lspconfig")["bashls"].setup({
+  -- Language servers configuration
+  local default_options = {
     on_attach = utils.on_attach,
     capabilities = capabilities,
     flags = {
       debounce_text_changes = 100,
     },
-    root_dir = function(filename)
-      return require("lspconfig.util").root_pattern(".git")(filename)
-        or require("lspconfig.util").path.dirname(filename)
-    end,
-  })
-
-  require("lspconfig")["ansiblels"].setup({
-    filetypes = { "yaml.ansible" },
-    on_attach = utils.on_attach,
-    capabilities = capabilities,
-    flags = {
-      debounce_text_changes = 100,
+  }
+  local lsp_servers = {
+    bashls = {
+      root_dir = function(filename)
+        return require("lspconfig.util").root_pattern(".git")(filename)
+          or require("lspconfig.util").path.dirname(filename)
+      end,
     },
-    root_dir = function(filename)
-      return require("lspconfig.util").root_pattern("ansible.cfg", ".git")(filename)
-        or require("lspconfig.util").path.dirname(filename)
-    end,
-    settings = {
-      ansible = {
+    ansiblels = {
+      filetypes = { "yaml.ansible" },
+      root_dir = function(filename)
+        return require("lspconfig.util").root_pattern("ansible.cfg", ".git")(filename)
+          or require("lspconfig.util").path.dirname(filename)
+      end,
+      settings = {
         ansible = {
-          useFullyQualifiedCollectionNames = false,
-        },
-        ansibleLint = {
-          arguments = "",
+          ansible = {
+            useFullyQualifiedCollectionNames = false,
+          },
+          ansibleLint = {
+            arguments = "",
+          },
         },
       },
     },
-  })
-
-  require("lspconfig")["jsonls"].setup({
-    on_attach = utils.on_attach,
-    capabilities = capabilities,
-    flags = {
-      debounce_text_changes = 100,
-    },
-    settings = {
-      json = {
-        schemas = {
-          {
-            fileMatch = {
-              "/nvim/snippets/*.json",
-              "!package.json",
-            },
-            schema = {
-              allowComments = false,
-              allowTrailingCommas = false,
-              type = "object",
-              description = "User snippet configuration",
-              defaultSnippets = {
-                {
-                  label = "Empty snippet",
-                  body = {
-                    ["${1:snippetName}"] = {
-                      prefix = "${2:prefix}",
-                      body = "${3:snippet}",
-                      description = "${4:description}",
-                    },
-                  },
-                },
+    jsonls = {
+      settings = {
+        json = {
+          schemas = {
+            {
+              fileMatch = {
+                "/nvim/snippets/*.json",
+                "!package.json",
               },
-              additionalProperties = {
+              schema = {
+                allowComments = false,
+                allowTrailingCommas = false,
                 type = "object",
-                required = { "body" },
-                additionalProperties = false,
+                description = "User snippet configuration",
                 defaultSnippets = {
                   {
-                    label = "Snippet",
+                    label = "Empty snippet",
                     body = {
-                      prefix = "${1:prefix}",
-                      body = "${2:snippet}",
-                      description = "${3:description}",
+                      ["${1:snippetName}"] = {
+                        prefix = "${2:prefix}",
+                        body = "${3:snippet}",
+                        description = "${4:description}",
+                      },
                     },
                   },
                 },
-                properties = {
-                  prefix = {
-                    description = "The prefix to use when selecting the snippet in intellisense",
-                    type = { "string", "array" },
+                additionalProperties = {
+                  type = "object",
+                  required = { "body" },
+                  additionalProperties = false,
+                  defaultSnippets = {
+                    {
+                      label = "Snippet",
+                      body = {
+                        prefix = "${1:prefix}",
+                        body = "${2:snippet}",
+                        description = "${3:description}",
+                      },
+                    },
                   },
-                  body = {
-                    markdownDescription = "The snippet content. Use `$1`, `${1:defaultText}` to define cursor positions, use `$0` for the final cursor position. Insert variable values with `${varName}` and `${varName:defaultText}`, e.g. `This is file: $TM_FILENAME`.",
-                    type = { "string", "array" },
-                    items = { type = "string" },
-                  },
-                  description = {
-                    description = "The snippet description.",
-                    type = { "string", "array" },
+                  properties = {
+                    prefix = {
+                      description = "The prefix to use when selecting the snippet in intellisense",
+                      type = { "string", "array" },
+                    },
+                    body = {
+                      markdownDescription = "The snippet content. Use `$1`, `${1:defaultText}` to define cursor positions, use `$0` for the final cursor position. Insert variable values with `${varName}` and `${varName:defaultText}`, e.g. `This is file: $TM_FILENAME`.",
+                      type = { "string", "array" },
+                      items = { type = "string" },
+                    },
+                    description = {
+                      description = "The snippet description.",
+                      type = { "string", "array" },
+                    },
                   },
                 },
               },
@@ -141,17 +122,21 @@ function lspconfig.config()
         },
       },
     },
-  })
+    pyright = {},
+  }
 
-  for _, lsp_server in ipairs({ "pyright" }) do
-    require("lspconfig")[lsp_server].setup({
-      on_attach = utils.on_attach,
-      capabilities = capabilities,
-      flags = {
-        debounce_text_changes = 100,
-      },
-    })
+  for lsp_server, options in pairs(lsp_servers) do
+    local final_options = vim.tbl_deep_extend("force", default_options, options)
+    require("lspconfig")[lsp_server].setup(final_options)
   end
+
+  -- Lua language server
+  local luadev = require("lua-dev").setup({
+    lspconfig = vim.tbl_extend("force", default_options, {
+      cmd = { "lua-language-server" },
+    }),
+  })
+  require("lspconfig").sumneko_lua.setup(luadev)
 end
 
 return lspconfig
