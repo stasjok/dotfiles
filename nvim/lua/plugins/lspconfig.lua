e4427c5634d8feb7cbf4f70626c87e50bf7bc96c
Stanislav Asunkin <1353637+stasjok@users.noreply.github.com>
2021-09-01 22:03:03 +0300

    style(nvim/lspconfig): add comentary before capabilities

--- nvim/lua/plugins/lspconfig.lua
+++ nvim/lua/plugins/lspconfig.lua
@@ -3,6 +3,7 @@ local utils = require("plugins.lspconfig.utils")
 local lspconfig = {}
 
 function lspconfig.config()
+  -- Capabilities
   local capabilities = vim.lsp.protocol.make_client_capabilities()
   capabilities = require("cmp_nvim_lsp").update_capabilities(capabilities)
 
