200b825f235de9139bbae32fc5fbd19ad8a66ee3
Stanislav Asunkin <1353637+stasjok@users.noreply.github.com>
2021-09-02 21:39:05 +0300

    refactor(nvim/lspconfig): require util at the top of ansiblels config

--- nvim/lua/plugins/lspconfig/ansiblels.lua
+++ nvim/lua/plugins/lspconfig/ansiblels.lua
@@ -1,10 +1,11 @@
+local util = require("lspconfig.util")
+
 local ansiblels = {}
 
 ansiblels.filetypes = { "yaml.ansible" }
 
 ansiblels.root_dir = function(filename)
-  return require("lspconfig.util").root_pattern("ansible.cfg", ".git")(filename)
-    or require("lspconfig.util").path.dirname(filename)
+  return util.root_pattern("ansible.cfg", ".git")(filename) or util.path.dirname(filename)
 end
 
 ansiblels.settings = {
