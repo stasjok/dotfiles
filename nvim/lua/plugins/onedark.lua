67c37d0fafe598b27decf3a54e493b9444efb580
Stanislav Asunkin <1353637+stasjok@users.noreply.github.com>
2021-09-03 18:26:52 +0300

    fix(nvim/colors): revert telescopematching highligh to default

--- nvim/lua/plugins/onedark.lua
+++ nvim/lua/plugins/onedark.lua
@@ -4,6 +4,8 @@ function onedark.config()
   require("onedark").setup({
     keywordStyle = "NONE",
   })
+  -- Restore TelescopeMatching highlight to default
+  vim.cmd("highlight! link TelescopeMatching Special")
 end
 
 return onedark
