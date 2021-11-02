7202c887274aa6c9f4e67da39c7c1f00ef131b2e
Stanislav Asunkin <1353637+stasjok@users.noreply.github.com>
2021-11-02 16:06:49 +0300

    feat(fish): add aliases for running salt commands via ssh

---
+++ fish/functions/salt.fish
@@ -0,0 +1,5 @@
+function (status basename | string split -r -m 1 -f 1 .) -d "Run salt commands via SSH"
+    set -l command (status function)
+    test -z $salt_hostname; and read -U -P "Enter Salt hostname: " salt_hostname
+    ssh root@$salt_hostname $command --force-color (string escape -- $argv)
+end
