060fd7b2a9e82e63459cdb0ad377576925a22255
Stanislav Asunkin <1353637+stasjok@users.noreply.github.com>
2022-03-27 17:14:59 +0300

    refactor(onedark): use official scheme configurator

--- nvim/lua/plugin_configs/onedark.lua
+++ nvim/lua/plugin_configs/onedark.lua
@@ -1,19 +1,15 @@
 local onedark = {}
 
 function onedark.config()
-  -- Apply configuration
-  require("onedark.config").apply_configuration({
+  require("onedark").setup({
     keyword_style = "NONE",
-    function_style = "NONE",
+    hide_inactive_statusline = false,
+    overrides = function()
+      return {
+        TelescopeMatching = { link = "String" },
+      }
+    end,
   })
-  -- Load colors
-  local theme = require("onedark.theme").setup(require("onedark.config").config)
-
-  -- Modify theme
-  theme.plugins.TelescopeMatching = { link = "Special" }
-
-  -- Apply colorscheme
-  require("onedark.util").load(theme)
 end
 
 return onedark
