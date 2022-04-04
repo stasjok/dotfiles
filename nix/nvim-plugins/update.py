904d19e027a5275abbb5921fee9dd16b7ff540cb
Stanislav Asunkin <1353637+stasjok@users.noreply.github.com>
2022-04-04 20:28:59 +0300

    refactor(nix): rewrite script for updating plugins

--- nix/nvim-plugins/update.py
+++ nix/nvim-plugins/update.py
@@ -3,35 +3,84 @@
 from datetime import date
 import json
 from pathlib import Path
+import re
 from subprocess import PIPE, run
+from urllib.parse import parse_qs, urlsplit, urlunsplit
 
 
 DIR = Path(__file__).parent
-PLUGINS = Path.joinpath(DIR, "plugins.json")
+PLUGINS = Path.joinpath(DIR, "plugins")
 GENERATED = Path.joinpath(DIR, "generated.nix")
 HEADER = """{ buildVimPlugin }:
 
 {
 """
-TAIL = """}"""
+TAIL = "}"
 TEMPLATE = """  {name} = buildVimPlugin {{
     pname = "{pname}";
     version = "{version}";
     src = builtins.fetchTree {{
-      {src}
+{src}
     }};
   }};
 """
-GITHUB_TEMPLATE = """type = "github";
-      owner = "{owner}";
-      repo = "{repo}";
-      rev = "{rev}";
-      narHash = "{hash}";"""
-GIT_TEMPLATE = """type = "git";
-      url = {url};
-      ref = "{ref}";
-      rev = "{rev}";
-      narHash = "{hash}";"""
+
+
+def process_line(line: str) -> str:
+    parsed = urlsplit(line)
+    if parsed.netloc:
+        # Git
+        args = {
+            "type": "git",
+            "url": urlunsplit(parsed._replace(query="")),
+        }
+        name = parsed.path.split("/")[-1].removesuffix(".git")
+    else:
+        # GitHub
+        parts = parsed.path.split("/")
+        args = {
+            "type": "github",
+            "owner": parts[0],
+            "repo": parts[1],
+        }
+        try:
+            args.update(rev_or_ref(parts[2]))
+        except IndexError:
+            pass
+        name = parts[1]
+    args.update(parse_query(parsed.query))
+
+    nix_args = get_nix_args(args)
+    nix_output = run(nix_args, stdout=PIPE, check=True)
+    plugin_src = json.loads(nix_output.stdout)
+    args["rev"] = plugin_src["rev"]
+    args["narHash"] = plugin_src["narHash"]
+    plugin_args = {
+        "name": escape_name(name),
+        "pname": name,
+        "version": date.fromtimestamp(plugin_src["lastModified"]),
+        "src": to_nix_attrs(args, 6),
+    }
+    return TEMPLATE.format(**plugin_args)
+
+
+def rev_or_ref(rev: str) -> dict[str, str]:
+    if re.fullmatch("[a-f0-9]{40}", rev):
+        t = "rev"
+    else:
+        t = "ref"
+    return {t: rev}
+
+
+def parse_query(query: str) -> dict[str, str]:
+    return dict(map(lambda kv: (kv[0], kv[1][-1]), parse_qs(query).items()))
+
+
+def to_nix_attrs(attrs: dict[str, str], indent=0) -> str:
+    lines = []
+    for k, v in attrs.items():
+        lines.append(f'{"":{indent}}{k} = "{v}";')
+    return "\n".join(lines)
 
 
 def get_nix_args(args: dict[str, str]) -> list[str]:
@@ -54,58 +103,12 @@ def escape_name(name: str) -> str:
 
 
 def main() -> None:
-    with Path.joinpath(DIR, "plugins.json").open() as f:
-        plugins = json.load(f)
     result = HEADER
-    # Github plugins
-    github_plugins: list[str] = plugins.get("github", [])
-    for plugin in github_plugins:
-        parts = plugin.split("/")
-        src_args = {
-            "type": "github",
-            "owner": parts[0],
-            "repo": parts[1],
-        }
-        try:
-            src_args["rev"] = parts[2]
-        except IndexError:
-            pass
-        nix_args = get_nix_args(src_args)
-        nix_output = run(nix_args, stdout=PIPE, check=True)
-        plugin_src = json.loads(nix_output.stdout)
-        src_args["hash"] = plugin_src["narHash"]
-        src_args["rev"] = plugin_src["rev"]
-        repo = src_args["repo"]
-        plugin_args = {
-            "name": escape_name(repo),
-            "pname": repo,
-            "version": date.fromtimestamp(plugin_src["lastModified"]),
-            "src": GITHUB_TEMPLATE.format(**src_args),
-        }
-        result += TEMPLATE.format(**plugin_args)
-    # Git plugins
-    git_plugins: list[dict[str, str]] = plugins.get("git", [])
-    for plugin in git_plugins:
-        src_args = plugin.copy()
-        src_args["type"] = "git"
-        nix_args = get_nix_args(src_args)
-        nix_output = run(nix_args, stdout=PIPE, check=True)
-        plugin_src = json.loads(nix_output.stdout)
-        src_args.update(
-            {
-                "ref": plugin.get("ref", "master"),
-                "rev": plugin_src["rev"],
-                "hash": plugin_src["narHash"],
-            }
-        )
-        pname = plugin["url"].split("/")[-1].removesuffix(".git")
-        plugin_args = {
-            "name": escape_name(pname),
-            "pname": pname,
-            "version": date.fromtimestamp(plugin_src["lastModified"]),
-            "src": GIT_TEMPLATE.format(**src_args),
-        }
-        result += TEMPLATE.format(**plugin_args)
+    with PLUGINS.open() as f:
+        for line in f:
+            l = line.strip()
+            if l and not l.startswith("#"):
+                result += process_line(l)
     result += TAIL
     Path(GENERATED).write_text(result)
 
