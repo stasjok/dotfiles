1e4b9a6526b8479467923f5813c866fc6a4ff81f
Stanislav Asunkin <1353637+stasjok@users.noreply.github.com>
2022-04-16 18:41:39 +0300

    chore(flake): update nixpkgs-unstable

--- flake.nix
+++ flake.nix
@@ -25,9 +25,9 @@
       type = "github";
       owner = "NixOS";
       repo = "nixpkgs";
-      # Released on 2022-03-27 03:47:33 via https://hydra.nixos.org/eval/1751103
-      rev = "30d3d79b7d3607d56546dd2a6b49e156ba0ec634";
-      narHash = "sha256-Ctij+dOi0ZZIfX5eMhgwugfvB+WZSrvVNAyAuANOsnQ=";
+      # Released on 2022-04-16 04:53:52 via https://hydra.nixos.org/eval/1755745
+      rev = "0b43a436fbbf96ecda249c4b8df5a349fe9f5e15";
+      narHash = "sha256-3lmFtbxH3BSUYkq7gTtoW9dLqkIAkjXWhlncfdx0wCk=";
     };
 
   };
