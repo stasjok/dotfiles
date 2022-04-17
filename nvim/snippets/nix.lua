f42959269622698fed2253dedf82f7812e0c8291
Stanislav Asunkin <1353637+stasjok@users.noreply.github.com>
2022-04-17 12:41:50 +0300

    feat(nvim/snippets/nix): expand ; only in attrset and let

--- nvim/snippets/nix.lua
+++ nvim/snippets/nix.lua
@@ -4,6 +4,8 @@ local c = require("luasnip.nodes.choiceNode").C
 local r = require("luasnip.nodes.restoreNode").R
 local fmt = require("luasnip.extras.fmt").fmt
 
+local ts_utils = require("nvim-treesitter.ts_utils")
+
 local function cr(pos, choices)
   return c(pos, choices, { restore_cursor = true })
 end
@@ -12,9 +14,23 @@ local function fmte(str, nodes)
   return fmt(str, nodes, { trim_empty = false, dedent = false })
 end
 
+local function is_in_ts_node(nodes)
+  if type(nodes) == "string" then
+    nodes = { nodes }
+  end
+  local node_at_cursor = ts_utils.get_node_at_cursor()
+  if node_at_cursor then
+    return vim.tbl_contains(nodes, node_at_cursor:type())
+  end
+end
+
 return {
   -- attr = value;
-  s({ trig = ";", hidden = true }, fmt("{} = {};", { i(1, "attr"), i(2, "value") })),
+  s({ trig = ";", hidden = true }, fmt("{} = {};", { i(1, "attr"), i(2, "value") }), {
+    condition = function()
+      return is_in_ts_node({ "attrset", "let" })
+    end,
+  }),
   -- ''
   --   $1
   -- ''
