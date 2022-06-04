0650536bb130037e599b6bc2359b900ddc15fdac
Stanislav Asunkin <1353637+stasjok@users.noreply.github.com>
2022-06-04 19:22:09 +0300

    feat(nvim/terminal): automatically enter insert mode after activating

--- nvim/plugin/terminal.lua
+++ nvim/plugin/terminal.lua
@@ -2,13 +2,13 @@ local buf_delete = vim.api.nvim_buf_delete
 local buf_is_valid = vim.api.nvim_buf_is_valid
 local create_buf = vim.api.nvim_create_buf
 local set_current_buf = vim.api.nvim_set_current_buf
-local command = vim.api.nvim_command
+local create_autocmd = vim.api.nvim_create_autocmd
 local map = vim.keymap.set
 
 vim.cmd([[
 augroup terminal
   autocmd!
-  autocmd TermOpen * setlocal nonumber norelativenumber sidescrolloff=0 signcolumn=auto | startinsert
+  autocmd TermOpen * setlocal nonumber norelativenumber sidescrolloff=0 signcolumn=auto
 augroup END
 ]])
 
@@ -20,15 +20,17 @@ end
 local function terminal_open()
   if _G._my_terminal_buffer == nil or not buf_is_valid(_G._my_terminal_buffer) then
     _G._my_terminal_buffer = create_buf(true, false)
+    create_autocmd("BufEnter", {
+      buffer = _G._my_terminal_buffer,
+      command = "startinsert",
+    })
     set_current_buf(_G._my_terminal_buffer)
     vim.fn.termopen("fish", {
       on_exit = on_exit,
       env = { XDG_DATA_DIRS = vim.env.HOME .. "/.nix-profile/share" },
     })
-    command("startinsert")
   else
     set_current_buf(_G._my_terminal_buffer)
-    command("startinsert")
   end
 end
 
