1d936ca522d811e758a7c26dfa739a2742e4fd17
Stanislav Asunkin <1353637+stasjok@users.noreply.github.com>
2022-06-13 20:54:28 +0300

    fix(vim-plugins): update luasnip to fix a crash with on the fly snippets

--- nix/vim-plugins/default.nix
+++ nix/vim-plugins/default.nix
@@ -7,6 +7,14 @@ generated // {
   impatient-nvim = vimPlugins.impatient-nvim.overrideAttrs (_: {
     patches = [ ./patches/impatient-nvim/add_luacache_suffix.patch ];
   });
+  luasnip = vimPlugins.luasnip.overrideAttrs (_: {
+    src = fetchTree {
+      type = "github";
+      owner = "L3MON4D3";
+      repo = "LuaSnip";
+      rev = "533e9fd880f208abe0a82471ff43e60b7fafdb24";
+    };
+  });
   lua-dev-nvim = generated.lua-dev-nvim.overrideAttrs (_: {
     patches = [ ./patches/lua-dev-nvim/fix_library.patch ];
   });
