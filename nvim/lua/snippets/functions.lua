2ca12bfa597d75dd5679266d63e13546eae44bc1
Stanislav Asunkin <1353637+stasjok@users.noreply.github.com>
2022-06-13 21:23:18 +0300

    fix(nvim/snippets): fix select_dedent for nested snippets

--- nvim/lua/snippets/functions.lua
+++ nvim/lua/snippets/functions.lua
@@ -5,7 +5,13 @@ local functions = {}
 ---@param parent table The immediate parent of the functionNode
 ---@return string
 function functions.select_dedent(_, parent)
-  return parent.env.SELECT_DEDENT
+  while parent do
+    if parent.env then
+      return parent.env.SELECT_DEDENT
+    end
+    parent = parent.parent
+  end
+  return ""
 end
 
 return functions
