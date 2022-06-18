5007ee58b11ddc970b867b7f0fc9be5173f14302
Stanislav Asunkin <1353637+stasjok@users.noreply.github.com>
2022-06-18 19:45:28 +0300

    refactor(nvim/snippets/jinja): heavily refactor jinja statements

--- nvim/snippets/jinja.lua
+++ nvim/snippets/jinja.lua
@@ -27,6 +27,141 @@ local show_conds = require("snippets.show_conditions")
 
 local snippets = {}
 
+---Returns `true` if one of the `strings` are found in current file path
+---@param strings string|string[]
+---@return boolean
+local function match_file_path(strings)
+  strings = type(strings) == "table" and strings or { strings }
+  ---@diagnostic disable-next-line: missing-parameter
+  local path = vim.fn.expand("%:p:h")
+  for _, str in ipairs(strings) do
+    if path:find(str, 1, true) then
+      return true
+    end
+  end
+  return false
+end
+
+---Returns `true` if current jinja file is for SaltStack
+---@return boolean
+local function is_salt()
+  return vim.bo.filetype == "sls" or match_file_path({ "salt", "formula" })
+end
+
+---Returns `true` if current jinja file is for Ansible
+---@return boolean
+local function is_ansible()
+  return vim.bo.filetype == "yaml.ansible" or match_file_path({ "ansible", "role" })
+end
+
+---Returns a function for getting boolean option from a vim variable
+---@param var string Variable for storing option
+---@param default boolean | fun(): boolean Default value if variable is unset
+---@return fun(): boolean
+local function get_option(var, default)
+  return function()
+    local opt = vim.b[var]
+    if opt ~= nil then
+      if type(opt) ~= "boolean" then
+        opt = opt ~= 0
+      end
+    else
+      if type(default) == "function" then
+        opt = default()
+      else
+        opt = default
+      end
+      vim.b[var] = opt
+    end
+    return opt
+  end
+end
+
+---Is `trim_blocks` enabled?
+local get_trim_block = get_option("jinja_trim_blocks", is_ansible)
+---Is `lstrip_blocks` enabled?
+local get_lstrip_blocks = get_option("jinja_lstrip_blocks", false)
+
+---A function for dynamicNode that returns jinja block start
+---@return table
+local function block_start()
+  local trim_blocks, lstrip_blocks = get_trim_block(), get_lstrip_blocks()
+  local block = trim_blocks and "{% " or "{%- "
+  local nodes = { t(block) }
+  if not lstrip_blocks then
+    table.insert(nodes, i(1))
+  end
+  return sn(nil, nodes)
+end
+
+---@alias JinjaGeneratorOpts {no_space?: boolean, end_statement?: string, right_trim?: boolean, condition?: function, show_condition?: function}
+
+---Returns a function for creating a snippet for jinja block
+---@param block boolean If `true` returns block statement
+---@param inline boolean If `true` returns inline statement
+local function jinja_statement_generator(block, inline)
+  ---Returns a Jinja statement snippet for LuaSnip
+  ---@param snip_args table First argument for LuaSnip snippet
+  ---@param nodes? table A nodes inside a statement
+  ---@param opts? JinjaGeneratorOpts Options affecting resulting snippet
+  ---@return table
+  return function(snip_args, nodes, opts)
+    opts = opts or {}
+    local statement = type(snip_args) == "string" and snip_args or snip_args.trig
+    -- local end_statement = opts.end_statement or ("end" .. statement)
+    local end_statement = opts.end_statement or ("end" .. "for")
+    if not opts.no_space and type(nodes) == "table" and not vim.tbl_isempty(nodes) then
+      statement = statement .. " "
+    end
+    local snip_nodes = {
+      t("{% " .. statement),
+      r(1, 1),
+      t(" %}"),
+    }
+    if block then
+      vim.list_extend(snip_nodes, {
+        r(2, 2),
+        t("{% " .. end_statement .. " %}"),
+      })
+    end
+    if not inline then
+      local inline_nodes = snip_nodes
+      local end_block = opts.right_trim and t(" -%}") or t(" %}")
+      snip_nodes = {
+        d(1, block_start),
+        t(statement),
+        r(2, 1),
+        end_block,
+      }
+      if block then
+        vim.list_extend(snip_nodes, {
+          t({ "", "" }),
+          r(3, 2),
+          t({ "", "" }),
+          rep(1),
+          t(end_statement .. " %}"),
+        })
+      end
+      snip_nodes = {
+        cr(1, {
+          snip_nodes,
+          inline_nodes,
+        }),
+      }
+    end
+    return s(snip_args, snip_nodes, {
+      stored = { nodes or {}, { f(select_dedent), i(1) } },
+      condition = opts.condition,
+      show_condition = opts.show_condition,
+    })
+  end
+end
+
+local jinja_statement = jinja_statement_generator(false, false)
+local jinja_inline_statement = jinja_statement_generator(false, true)
+local jinja_block = jinja_statement_generator(true, false)
+local jinja_inline_block = jinja_statement_generator(true, true)
+
 for statement, opts in pairs({
   ["for"] = {
     dscr = "For loop",
@@ -39,16 +174,15 @@ for statement, opts in pairs({
   macro = {
     dscr = "Macro definition",
     nodes = { i(1, "macro_name"), t("("), i(2), t(")") },
-    trim = "-",
-    inline = false,
+    right_trim = true,
   },
   call = {
     dscr = "Call block",
     nodes = { i(1, "macro_name"), t("("), i(2), t(")") },
-    trim = "-",
+    right_trim = true,
   },
   filter = {
-    dscr = "Filter section",
+    dscr = "Filter block",
     nodes = i(1, "filter_name"),
   },
   block = {
@@ -57,39 +191,19 @@ for statement, opts in pairs({
   },
   raw = {
     dscr = "Raw block",
-    nodes = {},
-    space = "",
   },
 }) do
-  table.insert(
-    snippets,
-    s({ trig = statement, dscr = opts.dscr or statement }, {
-      t("{%- " .. statement .. (opts.space or " ")),
-      wn(1, opts.nodes, true),
-      t({ " " .. (opts.trim or "") .. "%}", "" }),
-      f(select_dedent),
-      i(2),
-      t({ "", "{%- end" .. statement .. " %}" }),
-    }, {
-      condition = expand_conds.is_line_beginning,
-      show_condition = show_conds.is_line_beginning(),
-    })
-  )
-  if opts.inline ~= false then
-    table.insert(
-      snippets,
-      s({ trig = statement, dscr = "Inline " .. (opts.dscr or statement), wordTrig = false }, {
-        t("{% " .. statement .. (opts.space or " ")),
-        wn(1, opts.nodes, true),
-        t(" %}"),
-        f(select_dedent),
-        i(2),
-        t("{% end" .. statement .. " %}"),
-      }, {
-        show_condition = show_conds.is_not_line_beginning(),
-      })
-    )
-  end
+  local snip_opts = {
+    trig = statement,
+    dscr = opts.dscr or statement,
+  }
+  opts.condition = expand_conds.is_line_beginning
+  opts.show_condition = show_conds.is_line_beginning()
+  table.insert(snippets, jinja_block(vim.deepcopy(snip_opts), vim.deepcopy(opts.nodes), opts))
+  snip_opts.wordTrig = false
+  opts.condition = nil
+  opts.show_condition = show_conds.is_not_line_beginning()
+  table.insert(snippets, jinja_inline_block(snip_opts, opts.nodes, opts))
 end
 
 return snippets
