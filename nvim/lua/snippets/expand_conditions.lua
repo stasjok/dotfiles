5ed7aae7e226f2dd659e0ce72badc16816b6b184
Stanislav Asunkin <1353637+stasjok@users.noreply.github.com>
2022-07-03 15:48:05 +0300

    feat(nvim/snippets/jinja): create snippets for all stock jinja filters

--- nvim/lua/snippets/expand_conditions.lua
+++ nvim/lua/snippets/expand_conditions.lua
@@ -4,4 +4,13 @@ local expand_conditions = {}
 
 expand_conditions.is_line_beginning = luasnip_expand_conditions.line_begin
 
+---An `expand_condition` for LuaSnip matching if trigger is after a pattern
+---@param pattern string A pattern for matching
+---@return fun(line_to_cursor: string, matched_trigger: string): boolean
+function expand_conditions.is_after(pattern)
+  return function(line_to_cursor, matched_trigger)
+    return line_to_cursor:sub(1, -(#matched_trigger + 1)):match(pattern .. "$")
+  end
+end
+
 return expand_conditions
--- nvim/lua/snippets/show_conditions.lua
+++ nvim/lua/snippets/show_conditions.lua
@@ -21,4 +21,15 @@ function show_conditions.is_not_line_beginning(character_class)
   end
 end
 
+---A `show_condition` for LuaSnip matching if a word is after a pattern
+---@param pattern string A pattern for matching
+---@param character_class? string A character class without surrounding `[]` matching word pattern. Default: `%w_`.
+---@return fun(line_to_cursor: string): boolean
+function show_conditions.is_after(pattern, character_class)
+  character_class = character_class or "%w_"
+  return function(line_to_cursor)
+    return line_to_cursor:match(pattern .. "[" .. character_class .. "]*$")
+  end
+end
+
 return show_conditions
