683c9ab99683492a25d33ce4d47a37f70fe7524d
Stanislav Asunkin <1353637+stasjok@users.noreply.github.com>
2022-07-16 12:34:45 +0300

    refactor(nvim/lua): rework sumneko_lua settings

--- nvim/lua/plugin_configs/lspconfig/sumneko_lua.lua
+++ nvim/lua/plugin_configs/lspconfig/sumneko_lua.lua
@@ -1,18 +1,64 @@
+local util = require("lspconfig.util")
 local on_attach = require("plugin_configs.lspconfig.utils").on_attach
+local get_runtime = vim.api.nvim__get_runtime
+local fs_realpath = vim.loop.fs_realpath
 
-local luadev = require("lua-dev").setup({
-  lspconfig = {
-    cmd = { "lua-language-server" },
-    settings = {
-      Lua = {
-        completion = { callSnippet = "Disable" },
-      },
+local sumneko_lua = {}
+
+local root_files = {
+  "lua",
+  ".luarc.json",
+  ".luacheckrc",
+  ".stylua.toml",
+  "stylua.toml",
+  "selene.toml",
+}
+
+function sumneko_lua.root_dir(fname)
+  return util.root_pattern(unpack(root_files))(fname) or util.find_git_ancestor(fname)
+end
+
+function sumneko_lua.on_attach(client, buffer)
+  on_attach(client, buffer)
+  client.resolved_capabilities.document_formatting = false
+end
+
+local library = get_runtime({ "" }, true, { is_lua = true })
+for i = 1, #library do
+  library[i] = fs_realpath(library[i])
+end
+
+sumneko_lua.settings = {
+  Lua = {
+    completion = {
+      showWord = "Disable",
     },
-    on_attach = function(client, buffer)
-      on_attach(client, buffer)
-      client.resolved_capabilities.document_formatting = false
-    end,
+    format = {
+      enable = false,
+    },
+    hint = {
+      enable = false,
+    },
+    runtime = {
+      version = "LuaJIT",
+      path = { "lua/?.lua", "lua/?/init.lua" },
+      pathStrict = true,
+    },
+    semantic = {
+      enable = false,
+    },
+    telemetry = {
+      enable = false,
+    },
+    window = {
+      progressBar = false,
+      statusBar = false,
+    },
+    workspace = {
+      checkThirdParty = false,
+      library = library,
+    },
   },
-})
+}
 
-return luadev
+return sumneko_lua
