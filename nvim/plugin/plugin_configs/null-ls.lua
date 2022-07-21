55774397358ef97bcf1f3d6053c1228b0bd860c7
Stanislav Asunkin <1353637+stasjok@users.noreply.github.com>
2022-07-21 09:14:46 +0300

    fix(nvim/null-ls): disable shellcheck
    bash-language-server supports it now.

--- nvim/plugin/plugin_configs/null-ls.lua
+++ nvim/plugin/plugin_configs/null-ls.lua
@@ -1,10 +1,8 @@
-local diagnostics = require("null-ls").builtins.diagnostics
 local formatting = require("null-ls").builtins.formatting
 
 -- Configuration of null-ls
 require("null-ls").setup({
   sources = {
-    diagnostics.shellcheck,
     formatting.shfmt,
     formatting.stylua,
     formatting.black,
