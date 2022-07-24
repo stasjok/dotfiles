8320ac416fc99cdde3a10cea3c6e806d17533d5b
Stanislav Asunkin <1353637+stasjok@users.noreply.github.com>
2022-07-24 16:39:05 +0300

    refactor(nvim/lua): switch mappings and autocmds to lua api

--- nvim/ftplugin/lua.lua
+++ nvim/ftplugin/lua.lua
@@ -1,15 +1,19 @@
 local lspconfig = require("lspconfig")
 local lsputil = require("lspconfig.util")
+local map = vim.keymap.set
+local buf_get_name = vim.api.nvim_buf_get_name
+local create_augroup = vim.api.nvim_create_augroup
+local clear_autocmds = vim.api.nvim_clear_autocmds
+local create_autocmd = vim.api.nvim_create_autocmd
+local formatting_sync = vim.lsp.buf.formatting_sync
 
 vim.opt.shiftwidth = 2
 
 -- Source current file or selected lines
-for _, m in ipairs({ "n", "x" }) do
-  vim.api.nvim_buf_set_keymap(0, m, "<LocalLeader>s", ":source<CR>", { noremap = true })
-end
+map({ "n", "x" }, "<LocalLeader>s", ":source<CR>", { buffer = true })
 
 -- Auto-format on save
-local root_dir = lspconfig["sumneko_lua"].get_root_dir(vim.api.nvim_buf_get_name(0))
+local root_dir = lspconfig["sumneko_lua"].get_root_dir(buf_get_name(0))
 local stylua_conf_exists = root_dir
   and (
     lsputil.path.exists(root_dir .. "/stylua.toml")
@@ -17,10 +21,14 @@ local stylua_conf_exists = root_dir
   )
 
 if stylua_conf_exists then
-  vim.cmd([[
-    augroup LuaAutoFormatting
-    autocmd! * <buffer>
-    autocmd BufWritePre <buffer> lua vim.lsp.buf.formatting_sync(nil, 1000)
-    augroup END
-  ]])
+  local augroup = create_augroup("LuaAutoFormat", { clear = false })
+  clear_autocmds({ group = augroup, buffer = 0 })
+  create_autocmd("BufWritePre", {
+    desc = "Format on save",
+    group = augroup,
+    buffer = 0,
+    callback = function()
+      formatting_sync({}, 1000)
+    end,
+  })
 end
