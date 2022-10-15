b194be832a994678274281b1dc64f72005aec435
Stanislav Asunkin <1353637+stasjok@users.noreply.github.com>
2022-10-15 22:42:47 +0300

    fix(nvim/null-ls): don't use on_attach callback
    Null-ls doesn't support most of the features, so I don't need any
    mappings. Mappings for formatting are set with format plugin.

--- nvim/plugin/plugin_configs/null-ls.lua
+++ nvim/plugin/plugin_configs/null-ls.lua
@@ -1,9 +1,7 @@
-local on_attach = require("plugin_configs.lspconfig.utils").on_attach
 local formatting = require("null-ls").builtins.formatting
 
 -- Configuration of null-ls
 require("null-ls").setup({
-  on_attach = on_attach,
   sources = {
     formatting.shfmt,
     formatting.stylua,
