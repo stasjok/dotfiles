8f71e38984ffeaf7aaff00c0edf9fc14f0e5c0b9
Stanislav Asunkin <1353637+stasjok@users.noreply.github.com>
2022-10-17 21:47:25 +0300

    fix(flake): patch cmp highlighting groups in onedark-nvim theme

--- nix/vim-plugins/default.nix
+++ nix/vim-plugins/default.nix
@@ -12,4 +12,8 @@ generated // {
   lua-dev-nvim = generated.lua-dev-nvim.overrideAttrs (_: {
     patches = [ ./patches/lua-dev-nvim/fix_library.patch ];
   });
+
+  onedark-nvim = generated.onedark-nvim.overrideAttrs (_: {
+    patches = [ ./patches/onedark-nvim/0001-fix-cmp-don-t-use-default-highlight-groups.patch ];
+  });
 }
