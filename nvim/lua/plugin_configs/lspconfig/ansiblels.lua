ff5709e4e158694c2bc48f73a99a6bd33576b091
Stanislav Asunkin <1353637+stasjok@users.noreply.github.com>
2022-10-18 15:27:45 +0300

    fix(nvim/ansible): disable fqcn at work

--- nvim/lua/plugin_configs/lspconfig/ansiblels.lua
+++ nvim/lua/plugin_configs/lspconfig/ansiblels.lua
@@ -8,16 +8,18 @@ ansiblels.root_dir = function(filename)
   return util.root_pattern("ansible.cfg", ".git")(filename) or util.path.dirname(filename)
 end
 
+local is_at_work = vim.env.USER == "admAsunkinSS"
+
 ansiblels.settings = {
   ansible = {
     ansible = {
-      useFullyQualifiedCollectionNames = true,
+      useFullyQualifiedCollectionNames = not is_at_work,
     },
     python = {
       interpreterPath = "python3",
     },
     completion = {
-      provideRedirectModules = true,
+      provideRedirectModules = is_at_work,
       provideModuleOptionAliases = true,
     },
   },
