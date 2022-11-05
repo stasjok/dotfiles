8791806dc3d8c5b4e008ab2bc7d0d95caa46d13e
Stanislav Asunkin <1353637+stasjok@users.noreply.github.com>
2022-11-05 22:50:54 +0300

    test(minitest_helpers): add some helper functions for child

--- tests/nvim/functional/test_nvim_test.lua
+++ tests/nvim/functional/test_nvim_test.lua
@@ -120,4 +120,150 @@ T["minimal_child"]["test_nvim_test.lua tests"] = function()
   eq(all_cases, {})
 end
 
+T["child_methods"] = new_set({
+  hooks = {
+    pre_case = child.setup,
+    post_once = child.stop,
+  },
+})
+
+T["child_methods"]["child.get_lines()"] = new_set({
+  hooks = {
+    pre_case = function()
+      child.api.nvim_buf_set_lines(0, 0, -1, true, { "a", "b", "c", "d" })
+    end,
+  },
+})
+
+T["child_methods"]["child.get_lines()"]["works"] = new_set({
+  parametrize = {
+    -- join option
+    { { "a", "b", "c", "d" } },
+    { { "a", "b", "c", "d" }, { join = false } },
+    { "a\nb\nc\nd", { join = true } },
+    -- line-range
+    { { "b", "c" }, { start = 1, finish = 3 } },
+    -- strict
+    { { "a", "b", "c", "d" }, { finish = 10, strict = false } },
+  },
+}, {
+  test = function(expectation, args)
+    eq(child.get_lines(args), expectation)
+  end,
+})
+
+T["child_methods"]["child.get_lines()"]["strict"] = function()
+  expect.error(child.get_lines, "Index out of bounds", { finish = 10 })
+end
+
+T["child_methods"]["child.get_lines()"]["buffer"] = function()
+  local buf = child.api.nvim_create_buf(false, true)
+  child.api.nvim_buf_set_lines(buf, 0, -1, true, { "1", "2" })
+  eq(child.get_lines({ buf = buf }), { "1", "2" })
+  eq(child.get_lines({ buf = 0 }), { "a", "b", "c", "d" })
+end
+
+T["child_methods"]["child.set_lines()"] = new_set({
+  hooks = {
+    pre_case = function()
+      child.api.nvim_buf_set_lines(0, 0, -1, true, { "a", "b" })
+    end,
+  },
+})
+
+T["child_methods"]["child.set_lines()"]["works"] = new_set({
+  parametrize = {
+    -- lines
+    { { "line1", "line2" }, { "line1", "line2" } },
+    { { "line1", "line2" }, "line1\nline2" },
+    -- line-range
+    { { "a", "line", "b" }, "line", { start = 1, finish = 1 } },
+    -- strict
+    { { "a", "line" }, "line", { start = 1, finish = 10, strict = false } },
+  },
+}, {
+  test = function(expectation, lines, args)
+    child.set_lines(lines, args)
+    eq(child.api.nvim_buf_get_lines(0, 0, -1, true), expectation)
+  end,
+})
+
+T["child_methods"]["child.set_lines()"]["strict"] = function()
+  expect.error(child.set_lines, "Index out of bounds", "line", { finish = 10 })
+end
+
+T["child_methods"]["child.set_lines()"]["buffer"] = function()
+  local buf = child.api.nvim_create_buf(false, true)
+  child.set_lines({ "1", "2" }, { buf = buf })
+  eq(child.api.nvim_buf_get_lines(buf, 0, -1, true), { "1", "2" })
+  eq(child.api.nvim_buf_get_lines(0, 0, -1, true), { "a", "b" })
+end
+
+T["child_methods"]["child.get_cursor()"] = new_set({
+  hooks = {
+    pre_case = function()
+      child.api.nvim_buf_set_lines(0, 0, -1, true, { "aaa", "bbb", "ccc" })
+    end,
+  },
+})
+
+T["child_methods"]["child.get_cursor()"]["works"] = new_set({
+  parametrize = {
+    { 1, 0 },
+    { 2, 2 },
+  },
+}, {
+  test = function(row, col)
+    child.api.nvim_win_set_cursor(0, { row, col })
+    eq({ child.get_cursor() }, { row, col })
+  end,
+})
+
+T["child_methods"]["child.get_cursor()"]["window"] = function()
+  child.cmd("split")
+  for index, win in ipairs(child.api.nvim_list_wins()) do
+    child.api.nvim_win_set_cursor(win, { index, index })
+    eq({ child.get_cursor(win) }, { index, index })
+  end
+end
+
+T["child_methods"]["child.set_cursor()"] = new_set({
+  hooks = {
+    pre_case = function()
+      child.api.nvim_buf_set_lines(0, 0, -1, true, { "aaa", "bbb", "ccc" })
+    end,
+  },
+})
+
+T["child_methods"]["child.set_cursor()"]["works"] = new_set({
+  parametrize = {
+    { 1, 0 },
+    { 2, 2 },
+  },
+}, {
+  test = function(row, col)
+    child.set_cursor(row, col)
+    eq(child.api.nvim_win_get_cursor(0), { row, col })
+  end,
+})
+
+T["child_methods"]["child.set_cursor()"]["window"] = function()
+  child.cmd("split")
+  for index, win in ipairs(child.api.nvim_list_wins()) do
+    child.set_cursor(index, index, win)
+    eq(child.api.nvim_win_get_cursor(win), { index, index })
+  end
+end
+
+T["child_methods"]["child.get_size()"] = function()
+  child.lua("vim.opt.columns = 20")
+  child.lua("vim.opt.lines = 10")
+  eq({ child.get_size() }, { 20, 10 })
+end
+
+T["child_methods"]["child.set_size()"] = function()
+  child.set_size(30, 20)
+  eq({ child.o.columns, child.o.lines }, { 30, 20 })
+end
+
 return T
--- tests/nvim/minitest_helpers.lua
+++ tests/nvim/minitest_helpers.lua
@@ -49,6 +49,98 @@ helpers.new_child = function(opts)
     child.bo.readonly = false
   end
 
+  local prevent_hanging = function(method)
+    if not child.is_blocked() then
+      return
+    end
+
+    local msg = string.format("Can not use `child.%s` because child process is blocked.", method)
+    error(msg)
+  end
+
+  ---@alias ChildSetLinesOpts { buf?: number, start?: number, finish?: number, strict?: boolean }
+
+  ---Set a line-range in the buffer of child neovim
+  ---@param arr string[] | string
+  ---@param opts? ChildSetLinesOpts
+  ---@diagnostic disable-next-line: redefined-local
+  child.set_lines = function(arr, opts)
+    prevent_hanging("set_lines")
+
+    if type(arr) == "string" then
+      arr = vim.split(arr, "\n")
+    end
+
+    opts = vim.tbl_extend("keep", opts or {}, {
+      buf = 0,
+      start = 0,
+      finish = -1,
+      strict = true,
+    }) --[[@as ChildSetLinesOpts]]
+
+    child.api.nvim_buf_set_lines(opts.buf, opts.start, opts.finish, opts.strict, arr)
+  end
+
+  ---@alias ChildGetLinesOpts { buf?: number, start?: number, finish?: number, strict?: boolean, join?: boolean }
+
+  ---Get a line-range from the buffer of child neovim
+  ---@param opts? ChildGetLinesOpts
+  ---@return string[] | string
+  ---@diagnostic disable-next-line: redefined-local
+  child.get_lines = function(opts)
+    prevent_hanging("get_lines")
+
+    opts = vim.tbl_extend("keep", opts or {}, {
+      buf = 0,
+      start = 0,
+      finish = -1,
+      strict = true,
+      join = false,
+    }) --[[@as ChildGetLinesOpts]]
+
+    local lines = child.api.nvim_buf_get_lines(opts.buf, opts.start, opts.finish, opts.strict)
+    return opts.join and table.concat(lines, "\n") or lines
+  end
+
+  ---Sets the (1,0)-indexed cursor position in the window of the child neovim
+  ---@param row integer
+  ---@param col integer
+  ---@param window integer
+  child.set_cursor = function(row, col, window)
+    prevent_hanging("set_cursor")
+
+    child.api.nvim_win_set_cursor(window or 0, { row, col })
+  end
+
+  ---Gets the (1,0)-indexed cursor position in the window of the child neovim
+  ---@param window integer
+  ---@return integer row
+  ---@return integer col
+  child.get_cursor = function(window)
+    prevent_hanging("get_cursor")
+
+    return unpack(child.api.nvim_win_get_cursor(window or 0))
+  end
+
+  ---Sets the screen size of the child neovim
+  ---@param columns integer
+  ---@param lines integer
+  child.set_size = function(columns, lines)
+    prevent_hanging("set_size")
+
+    child.o.columns = columns
+    child.o.lines = lines
+  end
+
+  ---Gets the screen size of the child neovim
+  ---@return integer columns
+  ---@return integer lines
+  child.get_size = function()
+    prevent_hanging("get_size")
+
+    return child.o.columns, child.o.lines
+  end
+
   return child
 end
 
