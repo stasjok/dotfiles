2bf038330617cb5983ea390e5e0126805601b9ba
Stanislav Asunkin <1353637+stasjok@users.noreply.github.com>
2022-11-19 16:23:10 +0300

    chore(flake): update nixpkgs

--- nix/vim-plugins/default.nix
+++ nix/vim-plugins/default.nix
@@ -7,7 +7,7 @@ let
   generated = import ./generated.nix { inherit (vimUtils) buildVimPluginFrom2Nix; };
 in
 generated // {
-  luassert = neovimUtils.buildNeovimPluginFrom2Nix { pname = "luassert"; };
+  luassert = neovimUtils.buildNeovimPluginFrom2Nix { pname = "luassert"; version = "1.9.0-1"; };
 
   lua-dev-nvim = generated.lua-dev-nvim.overrideAttrs (_: {
     patches = [ ./patches/lua-dev-nvim/fix_library.patch ];
