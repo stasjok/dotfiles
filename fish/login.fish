f3062446bc5fc3849c617dca4f4224985a0dbbbb
Stanislav Asunkin <1353637+stasjok@users.noreply.github.com>
2023-01-01 15:58:22 +0300

    refactor(fish): reorder login init commands

--- fish/login.fish
+++ fish/login.fish
@@ -1,12 +1,12 @@
+# Disable greeting
+set -U fish_greeting ''
+
+# Set up ssh-agent
+find_ssh_agent
+
 # Clear old universal variables, because home-manager sets global variables
 set -q -U LOCALE_ARCHIVE; and set -e -U LOCALE_ARCHIVE
 set -q -U NIX_SSL_CERT_FILE; and set -e -U NIX_SSL_CERT_FILE
 set -q -U XDG_DATA_DIRS; and set -e -U XDG_DATA_DIRS
 set -q -U MANPATH; and set -e -U MANPATH
 set -q -U EDITOR; and set -e -U EDITOR
-
-# Set up ssh-agent
-find_ssh_agent
-
-# Disable greeting
-set --universal fish_greeting ''
