ed82b193e9e1878a751030af4732b672d9bf6b2a
Stanislav Asunkin <1353637+stasjok@users.noreply.github.com>
2023-01-05 21:20:49 +0300

    feat(fzf): run fzf in tmux popup window

--- configs/fzf.nix
+++ configs/fzf.nix
@@ -3,5 +3,11 @@
     enable = true;
     # It's already configured from ~/.nix-profile/share/fish/vendor_conf.d
     enableFishIntegration = false;
+
+    # Tmux integration
+    tmux = {
+      enableShellIntegration = true;
+      shellIntegrationOptions = ["-p"];
+    };
   };
 }
