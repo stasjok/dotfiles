00a0da2a3eff21dc7d44cb042a183a5f548c60f2
Stanislav Asunkin <1353637+stasjok@users.noreply.github.com>
2023-01-08 14:26:23 +0300

    perf(nvim): improve performance in init.lua before impatient.nvim
    I turned out that sha256() is slow (4ms), so get rid of it.

--- nvim/plugin_configs/impatient-nvim.lua
+++ nvim/plugin_configs/impatient-nvim.lua
@@ -1,16 +1,19 @@
 -- Speed up loading Lua modules in Neovim
 do
-  local luacache_suffix = vim.fn.sha256(vim.o.runtimepath):sub(1, 7)
-  local stdcache = vim.fn.stdpath("cache")
-  _G.__luacache_config = {
-    chunks = {
-      enable = true,
-      path = string.format("%s/luacache_chunks_%s", stdcache, luacache_suffix),
-    },
-    modpaths = {
-      enable = true,
-      path = string.format("%s/luacache_modpaths_%s", stdcache, luacache_suffix),
-    },
-  }
+  local luacache_suffix = vim.o.runtimepath:match("%w+%-vim%-pack%-dir")
+  if luacache_suffix then
+    luacache_suffix = luacache_suffix:sub(1, 7)
+    local stdcache = vim.fn.stdpath("cache")
+    _G.__luacache_config = {
+      chunks = {
+        enable = true,
+        path = string.format("%s/luacache_chunks_%s", stdcache, luacache_suffix),
+      },
+      modpaths = {
+        enable = true,
+        path = string.format("%s/luacache_modpaths_%s", stdcache, luacache_suffix),
+      },
+    }
+  end
   require("impatient")
 end
