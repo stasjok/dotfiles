00b2a89b5b69d640e3ddcb4aecbfa693c310b48d
Stanislav Asunkin <1353637+stasjok@users.noreply.github.com>
2023-01-11 22:31:47 +0300

    fix(nvim): fix crash on large nix files

--- packages/vim-plugins/overrides.nix
+++ packages/vim-plugins/overrides.nix
@@ -1,1 +1,7 @@
-{}: final: prev: {}
+{}: final: prev: {
+  nvim-treesitter = prev.nvim-treesitter.overrideAttrs (oldAttrs: {
+    patches = [
+      ./nvim-treesitter/reduce-injection-false-positives.patch
+    ];
+  });
+}
