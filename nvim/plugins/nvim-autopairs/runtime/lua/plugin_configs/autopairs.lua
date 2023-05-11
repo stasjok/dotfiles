53a07217c883716de4ca369974dab75b00918ff0
Stanislav Asunkin <1353637+stasjok@users.noreply.github.com>
2023-05-11 20:30:10 +0300

    fix(nvim): fix auto-semicolon after let in rust for long lines

--- nvim/plugins/nvim-autopairs/runtime/lua/plugin_configs/autopairs.lua
+++ nvim/plugins/nvim-autopairs/runtime/lua/plugin_configs/autopairs.lua
@@ -202,7 +202,7 @@ local pairs = {
     :with_cr(cond.none()),
   Rule("=", ";", "rust")
     :with_pair(function(opts)
-      return cond.before_regex("let%s+[^=]+$", 40)(opts) and cond.after_regex("^%s*$", 10)(opts)
+      return cond.before_regex("let%s+[^=]+$", 100)(opts) and cond.after_regex("^%s*$", 10)(opts)
     end)
     :with_move(char_matches_end_pair)
     :with_cr(cond.none()),
