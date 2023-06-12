dd9fa9737b7def307fd45faaaf15e35fa9678d57
Stanislav Asunkin <1353637+stasjok@users.noreply.github.com>
2023-06-12 11:19:23 +0300

    fix(nvim): fix type annotations for `vim.api.nvim__get_runtime`

--- packages/vim-plugins/overrides.nix
+++ packages/vim-plugins/overrides.nix
@@ -1,2 +1,9 @@
-{}: final: prev: {
+{fetchpatch}: final: prev: {
+  # Fix some type annotations
+  neodev-nvim = prev.neodev-nvim.overrideAttrs (finalAttrs: prevAttrs: {
+    patches = fetchpatch {
+      url = "https://github.com/stasjok/neodev.nvim/commit/72a2320e6e6528144b5d8508883102fe40883e22.diff";
+      hash = "sha256-D3prfnROYtYNTWJm9/0Tj2FzdRS4TptjD08FuX8jXfg=";
+    };
+  });
 }
