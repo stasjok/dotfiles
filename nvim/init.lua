2364de6aa55c58ec725658bda919723c806cf381
Stanislav Asunkin <1353637+stasjok@users.noreply.github.com>
2023-06-18 13:10:26 +0300

    perf(nvim): don't require any modules before vim.loader

--- nvim/init.lua
+++ nvim/init.lua
@@ -1,7 +1,24 @@
-require("utils").set_rtp()
+-- Optimize `runtimepath` when running from Nix
+for _, dir in ipairs(vim.opt.runtimepath:get()) do
+  if vim.endswith(dir, "vim-pack-dir") then
+    local config_home = vim.fn.stdpath("config")
+    vim.opt.runtimepath = {
+      config_home,
+      dir,
+      vim.env.VIMRUNTIME,
+      config_home .. "/after",
+    }
+    vim.opt.packpath = {
+      dir,
+      vim.env.VIMRUNTIME,
+    }
+    break
+  end
+end
+
+-- Enable the experimental Lua module loader
+vim.loader.enable()
 
 -- Set <Leader> to <Space> and <LocalLeader> to `\`
 vim.g.mapleader = " "
 vim.g.maplocalleader = "\\"
-
-vim.loader.enable()
