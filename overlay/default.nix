f7e55ab7c1c3a53b55bd62060778e11ee2feef99
Stanislav Asunkin <1353637+stasjok@users.noreply.github.com>
2023-07-09 22:35:12 +0300

    refactor: update nixpkgs and remove unnecessary overrides

--- overlay/default.nix
+++ overlay/default.nix
@@ -31,9 +31,6 @@ final: prev: {
     };
   };
 
-  # Do not require unfree license of vscode
-  vscode-langservers-extracted = prev.vscode-langservers-extracted.override {vscode = final.vscodium;};
-
   # Pin ansible-language-server to v1.0.4 because of variable completion issue
   # https://github.com/ansible/ansible-language-server/issues/587
   ansible-language-server = prev.ansible-language-server.overrideAttrs (prevAttrs: rec {
@@ -63,39 +60,4 @@ final: prev: {
     };
     passthru = {inherit npmDeps;};
   });
-
-  # Support for proxy in fetchNpmDeps
-  prefetch-npm-deps = prev.prefetch-npm-deps.overrideAttrs (prevAttrs: {
-    nativeBuildInputs = prevAttrs.nativeBuildInputs ++ [final.pkg-config];
-    buildInputs = [final.curl];
-
-    patches = [
-      (final.fetchpatch {
-        url = "https://github.com/NixOS/nixpkgs/pull/240419/commits/aa2f51f0d2522b6eea996930e005b49a0a195798.diff";
-        stripLen = 4;
-        hash = "sha256-PW42TS7wuin1HrfdO6DvO10W/xLlDHr3TxdXVmzyKh8=";
-      })
-      (final.fetchpatch {
-        url = "https://github.com/NixOS/nixpkgs/pull/240419/commits/d2897e463dcf858bdba4261fc05a88701ebbd6e4.diff";
-        excludes = ["all-packages.nix" "default.nix"];
-        stripLen = 4;
-        hash = "sha256-SLes6fu3StHTAGqGZa5TzbqRXpAGrEcD1vTm2dLFFws=";
-      })
-    ];
-
-    cargoDeps = final.rustPlatform.importCargoLock {
-      lockFile = final.fetchurl {
-        url = "https://github.com/NixOS/nixpkgs/raw/d2897e463dcf858bdba4261fc05a88701ebbd6e4/pkgs/build-support/node/fetch-npm-deps/Cargo.lock";
-        hash = "sha256-7JTy9R7T5VAYsFfoZRdBtJppEckszmq/y3Zk1J1DX6g=";
-      };
-    };
-  });
-
-  fetchNpmDeps = let
-    fetch-npm-deps = final.fetchurl {
-      url = "https://github.com/NixOS/nixpkgs/raw/d2897e463dcf858bdba4261fc05a88701ebbd6e4/pkgs/build-support/node/fetch-npm-deps/default.nix";
-      hash = "sha256-jzqMeh21gy+FE7DFgZVLeSU70Wg8zEs/CVJQBlES81k=";
-    };
-  in
-    (prev.callPackage fetch-npm-deps {}).fetchNpmDeps;
 }
