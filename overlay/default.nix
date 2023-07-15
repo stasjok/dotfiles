7baa83310b6c916ae76fb126469786a72691787c
Stanislav Asunkin <1353637+stasjok@users.noreply.github.com>
2023-07-15 10:04:28 +0300

    refactor(overlay): update ansible-language-server

--- overlay/default.nix
+++ overlay/default.nix
@@ -31,31 +31,19 @@ final: prev: {
     };
   };
 
-  # Pin ansible-language-server to v1.0.4 because of variable completion issue
+  # Update ansible-language-server to fix variable completion issue
   # https://github.com/ansible/ansible-language-server/issues/587
   ansible-language-server = prev.ansible-language-server.overrideAttrs (prevAttrs: rec {
-    version = "1.0.4";
+    version = "1.2.0";
     src = prevAttrs.src.override {
       rev = "v${version}";
-      hash = "sha256-IBySScjfF2bIbiOv09uLMt9QH07zegm/W1vmGhdWxGY=";
+      hash = "sha256-5QzwDsWjuq/gMWFQEkl4sqvsqfxTOZhaFBMhjiiOZSY=";
     };
-    patches = [
-      # Fix ansible lint config parsing
-      (final.fetchpatch {
-        url = "https://github.com/ansible/ansible-language-server/pull/577/commits/e39e5580e82af40c9f2a279a471d0c29dba1db7a.diff";
-        hash = "sha256-FJW4PLsmJImNJSpD0RwqRX+HOI1kfnEQONBrSWh1irg=";
-      })
-      # Get module route for FQCN with more than 3 elements
-      (final.fetchpatch {
-        url = "https://github.com/ansible/ansible-language-server/pull/538/commits/687760b31202c5318445e6287382136ada636f69.diff";
-        hash = "sha256-dUnY99JMHKPdPNNFdY5bOGGFnOow31NygkAz3T17LUo=";
-      })
-    ];
 
-    npmDepsHash = "sha256-rJ1O2OsrJhTIfywK9/MRubwwcCmMbu61T4zyayg+mAU=";
+    npmDepsHash = "sha256-bzffCAGn0aYVoG8IDaXd5I3x3AnGl5urX7BaBKf0tVI=";
     npmDeps = final.fetchNpmDeps {
       name = "${prevAttrs.pname}-${version}-npm-deps";
-      inherit src patches;
+      inherit src;
       hash = npmDepsHash;
     };
     passthru = {inherit npmDeps;};
