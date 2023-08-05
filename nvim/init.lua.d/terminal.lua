2669c612e6388573fe855a8e62811750c0a94fdf
Stanislav Asunkin <1353637+stasjok@users.noreply.github.com>
2023-08-05 13:24:09 +0300

    refactor(nvim): don't alias api functions separately in terminal.lua

--- nvim/init.lua.d/terminal.lua
+++ nvim/init.lua.d/terminal.lua
@@ -1,22 +1,10 @@
-local buf_delete = vim.api.nvim_buf_delete
-local buf_is_valid = vim.api.nvim_buf_is_valid
-local create_buf = vim.api.nvim_create_buf
-local set_current_buf = vim.api.nvim_set_current_buf
-local buf_line_count = vim.api.nvim_buf_line_count
-local win_get_cursor = vim.api.nvim_win_get_cursor
-local buf_get_mark = vim.api.nvim_buf_get_mark
-local buf_get_lines = vim.api.nvim_buf_get_lines
-local create_augroup = vim.api.nvim_create_augroup
-local create_autocmd = vim.api.nvim_create_autocmd
-local exec_autocmds = vim.api.nvim_exec_autocmds
-local get_proc = vim.api.nvim_get_proc
-local get_proc_children = vim.api.nvim_get_proc_children
-local opt_local = vim.opt_local
 local api = vim.api
+local opt_local = vim.opt_local
+local cmd = vim.cmd
 local map = vim.keymap.set
 
-local augroup = create_augroup("terminal", {})
-create_autocmd("TermOpen", {
+local augroup = api.nvim_create_augroup("terminal", {})
+api.nvim_create_autocmd("TermOpen", {
   group = augroup,
   pattern = "*",
   desc = "Set options for terminal window",
@@ -29,23 +17,23 @@ create_autocmd("TermOpen", {
 })
 
 local function on_exit()
-  buf_delete(_G._my_terminal_buffer, { force = true })
+  api.nvim_buf_delete(_G._my_terminal_buffer, { force = true })
   _G._my_terminal_buffer = nil
 end
 
 local function term_start_insert(args)
   local buf_end = false
-  local line_count = buf_line_count(0)
-  local current_line_number = win_get_cursor(0)[1]
+  local line_count = api.nvim_buf_line_count(0)
+  local current_line_number = api.nvim_win_get_cursor(0)[1]
   -- After changing buffer win_get_cursor always returns 1, 0
   if current_line_number == 1 then
-    current_line_number = buf_get_mark(0, '"')[1]
+    current_line_number = api.nvim_buf_get_mark(0, '"')[1]
   end
   if line_count == current_line_number then
     buf_end = true
   elseif line_count - current_line_number < 70 then
     -- New terminal buffer is filled with empty lines
-    local lines = buf_get_lines(0, current_line_number, line_count, true)
+    local lines = api.nvim_buf_get_lines(0, current_line_number, line_count, true)
     buf_end = true
     for _, line in ipairs(lines) do
       if line ~= "" then
@@ -69,18 +57,18 @@ local function term_start_insert(args)
 end
 
 local function terminal_open()
-  if _G._my_terminal_buffer == nil or not buf_is_valid(_G._my_terminal_buffer) then
-    _G._my_terminal_buffer = create_buf(true, false)
-    create_autocmd("BufEnter", {
+  if _G._my_terminal_buffer == nil or not api.nvim_buf_is_valid(_G._my_terminal_buffer) then
+    _G._my_terminal_buffer = api.nvim_create_buf(true, false)
+    api.nvim_create_autocmd("BufEnter", {
       buffer = _G._my_terminal_buffer,
       callback = term_start_insert,
     })
-    set_current_buf(_G._my_terminal_buffer)
+    api.nvim_set_current_buf(_G._my_terminal_buffer)
     vim.fn.termopen("fish", {
       on_exit = on_exit,
     })
   else
-    set_current_buf(_G._my_terminal_buffer)
+    api.nvim_set_current_buf(_G._my_terminal_buffer)
   end
 end
 
@@ -91,10 +79,10 @@ local procs_which_uses_esc = {
 }
 
 local function is_proc_uses_esc(pid)
-  if procs_which_uses_esc[get_proc(pid).name] then
+  if procs_which_uses_esc[api.nvim_get_proc(pid).name] then
     return true
   end
-  for _, child_pid in ipairs(get_proc_children(pid)) do
+  for _, child_pid in ipairs(api.nvim_get_proc_children(pid)) do
     if is_proc_uses_esc(child_pid) then
       return true
     end
@@ -111,19 +99,19 @@ map("t", "<Esc>", terminal_esc, { expr = true })
 map("t", "<C-\\><Esc>", "<Esc>")
 map("t", "<M-PageUp>", "<C-\\><C-N><PageUp>")
 
-create_autocmd("VimEnter", {
+api.nvim_create_autocmd("VimEnter", {
   desc = "Open terminal automatically on startup",
   once = true,
   callback = function()
     if vim.o.columns >= 200 and api.nvim_buf_get_name(0) == "" then
-      vim.cmd.vsplit()
-      vim.cmd.wincmd("l")
+      cmd.vsplit()
+      cmd.wincmd("l")
       terminal_open()
-      exec_autocmds("TermOpen", {
+      api.nvim_exec_autocmds("TermOpen", {
         group = augroup,
         buffer = 0,
       })
-      vim.cmd.wincmd("h")
+      cmd.wincmd("h")
     end
   end,
 })
