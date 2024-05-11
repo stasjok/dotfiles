edb060a56305c275b995fc551130769224bd68ba
Stanislav Asunkin <1353637+stasjok@users.noreply.github.com>
2024-05-11 18:19:56 +0300

    fix(k9s): refactor and update skin
    * update skin parameter, old one doesn't work anymore
    * use catppuccin package from nixpkgs as source
    * set skin name in k9s settings

--- configs/k9s.nix
+++ configs/k9s.nix
@@ -1,21 +1,14 @@
 {pkgs, ...}: {
   programs.k9s = {
     enable = true;
-    skin = let
-      catppuccinRepo = fetchTree {
-        type = "github";
-        owner = "catppuccin";
-        repo = "k9s";
-        rev = "516f44dd1a6680357cb30d96f7e656b653aa5059";
-        narHash = "sha256-PtBJRBNbLkj7D2ko7ebpEjbfK9Ywjs7zbE+Y8FQVEfA=";
-      };
-      catppuccinMacchiatoSkin = builtins.fromJSON (builtins.readFile (
-        pkgs.runCommandNoCC "k9s-catppuccin-macchiato-skin.json" {} ''
-          ${pkgs.yq-go}/bin/yq -o json ${catppuccinRepo}/dist/macchiato.yml > $out
-        ''
-      ));
-    in
-      catppuccinMacchiatoSkin;
+
+    settings.k9s.ui.skin = "catppuccin-macchiato";
+
+    skins.catppuccin-macchiato = builtins.fromJSON (builtins.readFile (
+      pkgs.runCommandNoCC "k9s-catppuccin-macchiato-skin.json" {} ''
+        ${pkgs.yq-go}/bin/yq -o json ${pkgs.catppuccin}/k9s/catppuccin-macchiato.yaml > $out
+      ''
+    ));
   };
 
   # Force 24-bit colors
