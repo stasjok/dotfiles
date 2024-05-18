fefc890ea32ad9c7a156de8d939f4608b5af62fc
Stanislav Asunkin <1353637+stasjok@users.noreply.github.com>
2024-05-18 22:46:36 +0300

    refactor(overlay): override luajit and libluv only for neovim-patched
    It reduces a number of rebuilds and more packages
    are downloaded from nix cache.
    Also package sources are got directly from neovim deps.txt.

--- packages/lua-interpreters/default.nix
+++ packages/lua-interpreters/default.nix
@@ -7,17 +7,6 @@
 
   # Lua interpreter overrides
   interpreterOverrides = {
-    # luajit version matching neovim 0.10
-    luajit_2_1 = {
-      version = "2.1.1713484068";
-      src = fetchTree {
-        type = "github";
-        owner = "LuaJIT";
-        repo = "LuaJIT";
-        rev = "75e92777988017fe47c5eb290998021bbf972d1f";
-        narHash = "sha256-UnrsrXqAybmZve/Y86Q34Yn1TupNKm12wkJsfRpHoWw=";
-      };
-    };
   };
 
   mapAttrsFn = name: drv: let
--- packages/neovim/default.nix
+++ packages/neovim/default.nix
@@ -1,8 +1,92 @@
 {
   inputs,
+  lib,
+  pkgs,
+  fetchurl,
+  luajit,
   neovim-unwrapped,
-}:
-neovim-unwrapped.overrideAttrs {
-  pname = "neovim-patched";
-  src = inputs.neovim;
-}
+  runCommand,
+  stdenvNoCC,
+}: let
+  # Convert neovim's deps.txt to attrset
+  deps = lib.pipe "${inputs.neovim}/cmake.deps/deps.txt" [
+    builtins.readFile
+    (lib.splitString "\n")
+    (builtins.filter (s: s != ""))
+    (map (s: lib.splitString " " s))
+    (map (list: {
+      name = builtins.elemAt list 0;
+      value = builtins.elemAt list 1;
+    }))
+    builtins.listToAttrs
+  ];
+
+  # Get filename from URL without .tar extension and 'v' prefix
+  versionFromURL = url:
+    lib.pipe url [
+      (lib.flip lib.nameFromURL ".tar")
+      (lib.removePrefix "v")
+    ];
+
+  # Remove original and append overriden derivation to a list
+  replaceInput = prev: drv: builtins.filter (i: lib.getName i != lib.getName drv) prev ++ [drv];
+
+  # libuv
+  libuv = pkgs.libuv.overrideAttrs {
+    version = versionFromURL deps.LIBUV_URL;
+    src = fetchurl {
+      url = deps.LIBUV_URL;
+      sha256 = deps.LIBUV_SHA256;
+    };
+  };
+
+  # luv
+  luaPackageOverrides = final: prev: {
+    luv =
+      (prev.luaLib.overrideLuarocks prev.luv rec {
+        version = lib.removePrefix "luv-" (versionFromURL deps.LUV_URL);
+        src = fetchurl {
+          url = deps.LUV_URL;
+          sha256 = deps.LUV_SHA256;
+        };
+        # Update version in rockspec file
+        knownRockspec = runCommand "luv-${version}.rockspec" {} ''
+          sed 's/\(version = \)"\(.*\)"/\1"${version}"/' ${prev.luv.knownRockspec} >$out
+        '';
+      })
+      .overrideAttrs (prevAttrs: {
+        buildInputs = replaceInput prevAttrs.buildInputs libuv;
+      });
+    libluv = prev.libluv.overrideAttrs (prevAttrs: {
+      inherit (final.luv) version src;
+      buildInputs = replaceInput prevAttrs.buildInputs libuv;
+    });
+  };
+
+  # LuaJIT
+  lua = luajit.override rec {
+    version = let
+      relverFile = stdenvNoCC.mkDerivation {
+        name = "luajit-relver";
+        inherit src;
+        phases = ["unpackPhase" "installPhase"];
+        installPhase = "cp .relver $out";
+      };
+      relver = lib.fileContents relverFile;
+    in
+      "2.1." + relver;
+    src = fetchurl {
+      url = deps.LUAJIT_URL;
+      sha256 = deps.LUAJIT_SHA256;
+    };
+    packageOverrides = luaPackageOverrides;
+    self = lua;
+  };
+in
+  (neovim-unwrapped.override {
+    inherit lua libuv;
+  })
+  .overrideAttrs {
+    pname = "neovim-patched";
+    src = inputs.neovim;
+  }
