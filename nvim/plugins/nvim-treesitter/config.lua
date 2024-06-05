5e4c73051dfe1192727d772474be3fc9f5e04ecf
Stanislav Asunkin <1353637+stasjok@users.noreply.github.com>
2024-06-05 13:47:05 +0300

    fix(nvim): improve indent in fish
    tree-sitter indent doesn't work for unfinished blocks (no `end` block),
    so change to builtin indent since it's included in neovim now.

--- nvim/plugins/nvim-treesitter/config.lua
+++ nvim/plugins/nvim-treesitter/config.lua
@@ -8,7 +8,7 @@ require("nvim-treesitter.configs").setup({
   },
   indent = {
     enable = true,
-    disable = { "yaml" },
+    disable = { "yaml", "fish" },
   },
   incremental_selection = {
     enable = true,
