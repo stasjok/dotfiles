559a434710dfca965fa7f325e413763b9f388a07
Stanislav Asunkin <1353637+stasjok@users.noreply.github.com>
2024-06-23 17:18:13 +0300

    feat(nvim): add mapping to open diffview

--- nvim/plugins/diffview-nvim/config.lua
+++ nvim/plugins/diffview-nvim/config.lua
@@ -1,4 +1,5 @@
 local diffview = require("diffview")
+local map = vim.keymap.set
 
 diffview.setup({
   enhanced_diff_hl = true,
@@ -14,3 +15,7 @@ diffview.setup({
     },
   },
 })
+
+map("n", "<Leader>G", "<Cmd>DiffviewOpen<CR>")
+map("n", "<Leader>l", "<Cmd>DiffviewFileHistory<CR>")
+map("n", "<Leader>L", "<Cmd>DiffviewFileHistory %<CR>")
