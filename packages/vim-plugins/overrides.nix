9e78eb7b12d508e7c8685954d85bef116cf9e53c
Stanislav Asunkin <1353637+stasjok@users.noreply.github.com>
2024-06-30 19:05:02 +0300

    refactor(overlay): remove neodev-nvim override since I don't use it

--- packages/vim-plugins/overrides.nix
+++ packages/vim-plugins/overrides.nix
@@ -2,14 +2,6 @@
   fetchpatch,
   fetchFromGitHub,
 }: final: prev: {
-  # Fix some type annotations
-  neodev-nvim = prev.neodev-nvim.overrideAttrs (_: {
-    patches = fetchpatch {
-      url = "https://github.com/stasjok/neodev.nvim/commit/72a2320e6e6528144b5d8508883102fe40883e22.diff";
-      hash = "sha256-D3prfnROYtYNTWJm9/0Tj2FzdRS4TptjD08FuX8jXfg=";
-    };
-  });
-
   # Pin smart-splits.nvim to the version that doesn't run tmux commands on startup
   smart-splits-nvim = prev.smart-splits-nvim.overrideAttrs {
     src = fetchFromGitHub {
