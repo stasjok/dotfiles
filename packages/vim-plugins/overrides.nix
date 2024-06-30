ac70d47d744e12ee5b138833aed5b772b5810e5c
Stanislav Asunkin <1353637+stasjok@users.noreply.github.com>
2024-06-30 18:51:54 +0300

    perf(tmux): improve tmux navigation performance
    The method to detect neovim was changed in recent versions of
    smart-splits.nvim. Now it sets pane local variable on startup, and
    removes it on exit. Later another command was added to test varible in
    case of nested neovim. But every tmux command takes about 8-10 ms to run
    on my PC. Three commands total takes 25 ms. But this cost is only on
    startup. And it allows to just check variable in tmux instead of running
    external bash, ps and grep commands every time I press navigation key.
    
    In this commit I tried to get both. I pinned smart-splits.nvim to the
    older version without startup cost and changed tmux condition to use
    `pane_current_command` variable in tmux. On my very basic testing it
    works good enough. Obviously doesn't support nvim over ssh, but old `ps`
    solution didn't work for that case either.
    
    If `pane_current_command` won't work most of the time, then I'll have to
    revert and use `pane-is-vim` set by neovim.

--- packages/vim-plugins/overrides.nix
+++ packages/vim-plugins/overrides.nix
@@ -1,4 +1,7 @@
-{fetchpatch}: final: prev: {
+{
+  fetchpatch,
+  fetchFromGitHub,
+}: final: prev: {
   # Fix some type annotations
   neodev-nvim = prev.neodev-nvim.overrideAttrs (_: {
     patches = fetchpatch {
@@ -6,6 +9,16 @@
       hash = "sha256-D3prfnROYtYNTWJm9/0Tj2FzdRS4TptjD08FuX8jXfg=";
     };
   });
+
+  # Pin smart-splits.nvim to the version that doesn't run tmux commands on startup
+  smart-splits-nvim = prev.smart-splits-nvim.overrideAttrs {
+    src = fetchFromGitHub {
+      owner = "mrjones2014";
+      repo = "smart-splits.nvim";
+      rev = "159c4823e3a11c79bb65fc4b8560320c49f738f4";
+      hash = "sha256-S5I9nQcNGmjqZFn5jQkoG5Oh/mu8oSJpDZpAG07GytA=";
+    };
+  };
 
   # Remove tests because there are invalid lua files there
   nvim-treesitter = prev.nvim-treesitter.overrideAttrs (prev: {
