f4f0027f68979a001c4da792d1fa7188fd544547
Stanislav Asunkin <1353637+stasjok@users.noreply.github.com>
2024-07-23 19:05:47 +0300

    fix(overlay): fix building ansible-lint
    Since I pinned ansible to 2.12 earlier, more and more utils (like
    ansible-compat and ansible-lint) require the newer ansible version.
    Also there is a python incompatibility (python is updated to 3.12 in
    nixpkgs).
    
    Unpin ansible version for now.
    
    If I really would want to add ansible 2.12, I'll need to come up with
    another solution, e.g. installing it along with the latest version.
    Overriding globally is causing too much troubles when updating nixpkgs.

--- packages/python-packages/default.nix
+++ packages/python-packages/default.nix
@@ -1,43 +1,18 @@
-{
-  lib,
-  fetchurl,
-}: final: prev: {
-  # Ansible 2.12 (last support for python 2.6)
-  ansible-core = prev.ansible-core.overridePythonAttrs (prevAttrs: rec {
-    version = "2.12.10";
-    src = prevAttrs.src.override {
-      inherit version;
-      hash = "sha256-/rHfYXOM/B9eiTtCouwafeMpd9Z+hnB7Retj0MXDwjY=";
-    };
+{lib}: final: prev: {
+  # Intergate ansible_mitogen
+  ansible-core = prev.ansible-core.overridePythonAttrs (prevAttrs: {
     makeWrapperArgs = [
       "--suffix ANSIBLE_STRATEGY_PLUGINS : ${final.mitogen}/${final.python.sitePackages}/ansible_mitogen"
       "--set-default ANSIBLE_STRATEGY mitogen_linear"
     ];
     propagatedBuildInputs = prevAttrs.propagatedBuildInputs ++ [final.mitogen];
-    # Ansible 2.12 doesn't have 'packaging/cli-doc/build.py' script
-    # Reverts https://github.com/NixOS/nixpkgs/commit/e6b3cd9d23fa0078d73f1c5dc3b3e533e832b26c
-    postInstall = ''
-      installManPage docs/man/man1/*.1
-    '';
   });
 
-  # Ansible community version matching core
-  ansible = prev.ansible.overridePythonAttrs (prevAttrs: rec {
-    version = "5.10.0";
-    src = prevAttrs.src.override {
-      inherit version;
-      hash = "sha256-x39Vanw9mUj4ZjnFdCqohb4lp829o7+0GoMUtgozQeg=";
-    };
+  ansible = prev.ansible.overridePythonAttrs (prevAttrs: {
     propagatedBuildInputs = lib.unique (prevAttrs.propagatedBuildInputs
       ++ (with final; [
         # json_query filter
         jmespath
       ]));
   });
-
-  # Ansible-compat version supporting ansible 2.12
-  ansible-compat = final.callPackage (fetchurl {
-    url = "https://github.com/NixOS/nixpkgs/raw/59b1aef59071cae6e87859dc65de973d2cc595c0/pkgs/development/python-modules/ansible-compat/default.nix";
-    hash = "sha256-5hksuIc4miCVYmVNeQYfA1EDoA4wrOAJWT3iblvx8Jk=";
-  }) {};
 }
