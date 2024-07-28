55cb69136b9080e8f8f9623a7da461b42850ea32
Stanislav Asunkin <1353637+stasjok@users.noreply.github.com>
2024-07-28 14:53:48 +0300

    chore(flake): update nixpkgs
    Notable changes:
    * nixd is updated to 2.3.0. It can complete pkgs.| and lib.| now.

--- flake.nix
+++ flake.nix
@@ -2,7 +2,7 @@
   description = "Home Manager configuration of Stas";
 
   inputs = {
-    nixpkgs.url = "nixpkgs";
+    nixpkgs.url = "nixpkgs/038fb464fcfa79b4f08131b07f2d8c9a6bcc4160";
     home-manager = {
       url = "home-manager";
       inputs.nixpkgs.follows = "nixpkgs";
