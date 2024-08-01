8b9d4912c0d1324df5193fc103575283d4c3eb27
Stanislav Asunkin <1353637+stasjok@users.noreply.github.com>
2024-08-01 17:47:24 +0300

    test(nvim): add truthy and falsy expectations

--- tests/nvim/runtime/lua/test/expect.lua
+++ tests/nvim/runtime/lua/test/expect.lua
@@ -1,7 +1,11 @@
 local minitest = require("mini.test")
 
 -- Provide all MiniTest expectations
-M = vim.deepcopy(minitest.expect, true)
+local expect = vim.deepcopy(minitest.expect, true)
+
+--
+-- String matching expectations
+--
 
 local function match_fail_context(str, pattern, init, plain)
   if plain ~= nil then
@@ -36,7 +40,7 @@ local no_match_expectation = minitest.new_expectation(
 ---@param init? integer Where to start the match, default is 1
 ---@param plain? boolean Whether to match pattern literally
 ---@return true
-function M.match(string, pattern, init, plain)
+function expect.match(string, pattern, init, plain)
   return match_expectation(string, pattern, init, plain)
 end
 
@@ -47,8 +51,70 @@ end
 ---@param init? integer Where to start the match, default is 1
 ---@param plain? boolean Whether to match pattern literally
 ---@return true
-function M.no_match(string, pattern, init, plain)
+function expect.no_match(string, pattern, init, plain)
   return no_match_expectation(string, pattern, init, plain)
 end
 
-return M
+--
+-- Boolean expectations
+--
+
+local function value_with_message_context(value, message)
+  return vim
+    .iter({ message and "Error message: " .. message, "Observed value: " .. vim.inspect(value) })
+    :join("\n")
+end
+
+local truthy_expectation = minitest.new_expectation("truthy value", function(value)
+  return value
+end, value_with_message_context)
+
+local falsy_expectation = minitest.new_expectation("falsy value", function(value)
+  return not value
+end, value_with_message_context)
+
+local is_true_expectation = minitest.new_expectation("true value", function(value)
+  return value == true
+end, value_with_message_context)
+
+local is_false_expectation = minitest.new_expectation("false value", function(value)
+  return value == false
+end, value_with_message_context)
+
+--- Expect value to be truthy
+---
+---@param value any Value to be tested
+---@param message? string Optional error message
+---@return true
+function expect.truthy(value, message)
+  return truthy_expectation(value, message)
+end
+
+--- Expect value to be falsy
+---
+---@param value any Value to be tested
+---@param message? string Optional error message
+---@return true
+function expect.falsy(value, message)
+  return falsy_expectation(value, message)
+end
+
+--- Expect value to be true
+---
+---@param value any Value to be tested
+---@param message? string Optional error message
+---@return true
+function expect.is_true(value, message)
+  return is_true_expectation(value, message)
+end
+
+--- Expect value to be false
+---
+---@param value any Value to be tested
+---@param message? string Optional error message
+---@return true
+function expect.is_false(value, message)
+  return is_false_expectation(value, message)
+end
+
+return expect
--- tests/nvim/test/expect_test.lua
+++ tests/nvim/test/expect_test.lua
@@ -29,4 +29,85 @@ T["expect.no_match()"] = function()
   eq(expect.no_match("test-test", "test%-test", nil, true), true)
 end
 
+T["expect.truthy()"] = function()
+  -- Truthy
+  eq(expect.truthy(true), true)
+  eq(expect.truthy(0), true)
+  eq(expect.truthy(""), true)
+  eq(expect.truthy({}), true)
+  eq(expect.truthy(function() end), true)
+  -- Falsy
+  expect.error(expect.truthy, "Observed value: false", false)
+  expect.error(expect.truthy, "Observed value: nil", nil)
+  -- Message
+  expect.error(expect.truthy, "Error message: Want truthy.", false, "Want truthy.")
+  -- No message if not provided
+  local ok, err = pcall(expect.truthy, nil)
+  eq(ok, false)
+  ---@diagnostic disable-next-line: cast-type-mismatch
+  ---@cast err string
+  expect.match(err, "Observed value: nil")
+  expect.no_match(err, "Error message:")
+end
+
+T["expect.falsy()"] = function()
+  -- Falsy
+  eq(expect.falsy(false), true)
+  eq(expect.falsy(nil), true)
+  -- Falsy
+  expect.error(expect.falsy, "Observed value: true", true)
+  expect.error(expect.falsy, "Observed value: 1", 1)
+  expect.error(expect.falsy, [=[Observed value: ['"]a['"]]=], "a")
+  expect.error(expect.falsy, "Observed value: { ?1 ?}", { 1 })
+  -- Message
+  expect.error(expect.falsy, "Error message: Want falsy.", true, "Want falsy.")
+  -- No message if not provided
+  local ok, err = pcall(expect.falsy, true)
+  eq(ok, false)
+  ---@diagnostic disable-next-line: cast-type-mismatch
+  ---@cast err string
+  expect.match(err, "Observed value: true")
+  expect.no_match(err, "Error message:")
+end
+
+T["expect.is_true()"] = function()
+  -- True
+  eq(expect.is_true(true), true)
+  -- Not true
+  expect.error(expect.is_true, "Observed value: false", false)
+  expect.error(expect.is_true, "Observed value: nil", nil)
+  expect.error(expect.is_true, "Observed value: 1", 1)
+  expect.error(expect.is_true, [=[Observed value: ['"]a['"]]=], "a")
+  expect.error(expect.is_true, "Observed value: { ?1 ?}", { 1 })
+  -- Message
+  expect.error(expect.is_true, "Error message: Want true.", false, "Want true.")
+  -- No message if not provided
+  local ok, err = pcall(expect.is_true, false)
+  eq(ok, false)
+  ---@diagnostic disable-next-line: cast-type-mismatch
+  ---@cast err string
+  expect.match(err, "Observed value: false")
+  expect.no_match(err, "Error message:")
+end
+
+T["expect.is_false()"] = function()
+  -- False
+  eq(expect.is_false(false), true)
+  -- Not false
+  expect.error(expect.is_false, "Observed value: true", true)
+  expect.error(expect.is_false, "Observed value: nil", nil)
+  expect.error(expect.is_false, "Observed value: 0", 0)
+  expect.error(expect.is_false, [=[Observed value: ['"]a['"]]=], "a")
+  expect.error(expect.is_false, "Observed value: { ?}", {})
+  -- Message
+  expect.error(expect.is_false, "Error message: Want false.", true, "Want false.")
+  -- No message if not provided
+  local ok, err = pcall(expect.is_false, true)
+  eq(ok, false)
+  ---@diagnostic disable-next-line: cast-type-mismatch
+  ---@cast err string
+  expect.match(err, "Observed value: true")
+  expect.no_match(err, "Error message:")
+end
+
 return T
