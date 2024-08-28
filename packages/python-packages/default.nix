36e8e55181221982f2b98ee4147a8f33291f8bd8
Stanislav Asunkin <1353637+stasjok@users.noreply.github.com>
2024-08-28 13:22:12 +0300

    fix(overlay): pin ansible to 2.16
    Ansible 2.17 doesn't support python 2.7 and python 3.6 anymore, but
    python 3.6 is a default python on openSUSE, Oracle Linux <= 8 and some
    others. I can't drop its support.

--- packages/python-packages/default.nix
+++ packages/python-packages/default.nix
@@ -1,6 +1,13 @@
 {lib}: final: prev: {
   # Intergate ansible_mitogen
-  ansible-core = prev.ansible-core.overridePythonAttrs (prevAttrs: {
+  ansible-core = prev.ansible-core.overridePythonAttrs (prevAttrs: rec {
+    # Latest version supporting python 2.7 and 3.6
+    version = "2.16.10";
+    src = prevAttrs.src.override {
+      inherit version;
+      hash = "sha256-qLOHkKZ6+wL0npTx/Nqvz0dMlWeVkjO81xGYasx9SIQ=";
+    };
+
     makeWrapperArgs = [
       "--suffix ANSIBLE_STRATEGY_PLUGINS : ${final.mitogen}/${final.python.sitePackages}/ansible_mitogen"
       "--set-default ANSIBLE_STRATEGY mitogen_linear"
@@ -8,7 +15,14 @@
     propagatedBuildInputs = prevAttrs.propagatedBuildInputs ++ [final.mitogen];
   });
 
-  ansible = prev.ansible.overridePythonAttrs (prevAttrs: {
+  ansible = prev.ansible.overridePythonAttrs (prevAttrs: rec {
+    # Version matching ansible-core
+    version = "9.9.0";
+    src = prevAttrs.src.override {
+      inherit version;
+      hash = "sha256-1KhYxV+rD5dG7lWaIwowKz0pZzxOZzRQVW8AupEld84=";
+    };
+
     propagatedBuildInputs = lib.unique (prevAttrs.propagatedBuildInputs
       ++ (with final; [
         # json_query filter
