7df8c34094fb81a692583aeb3d45fa87c8136cd5
Stanislav Asunkin <1353637+stasjok@users.noreply.github.com>
2024-09-22 13:30:53 +0300

    fix(flake): update home-manager URL
    Since the commit where I began managing the Nix registry with
    home-manager also on my dev PC, it has been pinned to the commit
    specified in flake.lock. However, due to the use of an indirect URL,
    `nix flake update` has stopped updating home-manager. This commit uses
    the direct URL for home-manager to resolve the issue.

--- flake.nix
+++ flake.nix
@@ -4,7 +4,7 @@
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
     home-manager = {
-      url = "home-manager";
+      url = "github:nix-community/home-manager";
       inputs.nixpkgs.follows = "nixpkgs";
     };
     nixvim = {
