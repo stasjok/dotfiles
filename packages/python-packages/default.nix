47b59803714d2d33e4ad3df70265495288c945ec
Stanislav Asunkin <1353637+stasjok@users.noreply.github.com>
2024-11-16 11:56:57 +0300

    chore(flake): update ansible to the latest version of the 2.16

--- packages/python-packages/default.nix
+++ packages/python-packages/default.nix
@@ -3,10 +3,10 @@ final: prev: {
   # Intergate ansible_mitogen
   ansible-core = prev.ansible-core.overridePythonAttrs (prevAttrs: rec {
     # Latest version supporting python 2.7 and 3.6
-    version = "2.16.10";
+    version = "2.16.13";
     src = prevAttrs.src.override {
       inherit version;
-      hash = "sha256-qLOHkKZ6+wL0npTx/Nqvz0dMlWeVkjO81xGYasx9SIQ=";
+      hash = "sha256-RRlOEe/jxMDJuwsRK23cHKUCigG+a0G65ZNXaosRJ68=";
     };
 
     makeWrapperArgs = [
@@ -18,10 +18,10 @@ final: prev: {
 
   ansible = prev.ansible.overridePythonAttrs (prevAttrs: rec {
     # Version matching ansible-core
-    version = "9.9.0";
+    version = "9.12.0";
     src = prevAttrs.src.override {
       inherit version;
-      hash = "sha256-1KhYxV+rD5dG7lWaIwowKz0pZzxOZzRQVW8AupEld84=";
+      hash = "sha256-VFVzk/rldo7mQwSRxVt094Mdid0ZjT10Qx7arkQAQpg=";
     };
 
     propagatedBuildInputs = lib.unique (
