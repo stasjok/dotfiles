f2c955489a6414163787b1d96ec60e335211279c
Stanislav Asunkin <1353637+stasjok@users.noreply.github.com>
2025-05-24 11:33:37 +0300

    fix(home): fix man-db package
    There is no need to override db package for man-db anymore.
    Man-db from nixpkgs is already using gdbm.

--- linux.nix
+++ linux.nix
@@ -1,10 +1,5 @@
-{ pkgs, ... }:
 {
   # Enable settings that make Home Manager work better
   # on GNU/Linux distributions other than NixOS
   targets.genericLinux.enable = true;
-
-  # Build man package with support of the GNU gdbm database
-  # (same as in most GNU/Linux distributions)
-  programs.man.package = pkgs.man.override { db = pkgs.gdbm; };
 }
