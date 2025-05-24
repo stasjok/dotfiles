cff1f62038f3cabb4cfd97f443f1a2276633b329
Stanislav Asunkin <1353637+stasjok@users.noreply.github.com>
2025-05-24 17:43:36 +0300

    fix(nvim): update luasnip to fix deprecated usage of vim.validate

--- packages/lua-packages/default.nix
+++ packages/lua-packages/default.nix
@@ -1,4 +1,15 @@
-{ fetchurl }:
-final: prev:
-{
+{ fetchurl, fetchzip }:
+final: prev: {
+  # Update luasnip to fix deprecated usage of vim.validate
+  luasnip = final.luaLib.overrideLuarocks prev.luasnip {
+    version = "2.4.0-1";
+    knownRockspec = fetchurl {
+      url = "mirror://luarocks/luasnip-2.4.0-1.rockspec";
+      sha256 = "0rbv9z1bb8dy70mmy7w621zlhxcdv1g3bmmdxp012hicg7zrikyy";
+    };
+    src = fetchzip {
+      url = "https://github.com/L3MON4D3/LuaSnip/archive/v2.4.0.zip";
+      sha256 = "055mbyszd7gyxib4yi4wsiazs63p4d6ms3sp6x7xya7d0szfkl0n";
+    };
+  };
 }
