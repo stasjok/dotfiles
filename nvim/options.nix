d6aac73e4d6d853905b70a2beba5e71f7bd6db0c
Stanislav Asunkin <1353637+stasjok@users.noreply.github.com>
2025-05-25 17:34:55 +0300

    fix(nvim): disable terminal cursor blinking

--- nvim/options.nix
+++ nvim/options.nix
@@ -1,5 +1,12 @@
 {
   opts = {
+    # Disable terminal cursor blinking
+    guicursor = [
+      "n-v-c-sm:block"
+      "i-ci-ve:ver25"
+      "r-cr-o:hor20"
+      "t:block-TermCursor"
+    ];
     # Open all folds by default
     foldlevelstart = 99;
     # Default border style of floating windows
