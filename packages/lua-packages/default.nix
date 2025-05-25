88daf2bced2e0e89418bf5be33be8e308977e7e3
Stanislav Asunkin <1353637+stasjok@users.noreply.github.com>
2025-05-25 16:34:12 +0300

    fix(nvim): fix borders in telescope.nvim

--- packages/lua-packages/default.nix
+++ packages/lua-packages/default.nix
@@ -1,4 +1,8 @@
-{ fetchurl, fetchzip }:
+{
+  fetchpatch,
+  fetchurl,
+  fetchzip,
+}:
 final: prev: {
   # Update luasnip to fix deprecated usage of vim.validate
   luasnip = final.luaLib.overrideLuarocks prev.luasnip {
@@ -10,6 +14,15 @@ final: prev: {
     src = fetchzip {
       url = "https://github.com/L3MON4D3/LuaSnip/archive/v2.4.0.zip";
       sha256 = "055mbyszd7gyxib4yi4wsiazs63p4d6ms3sp6x7xya7d0szfkl0n";
+    };
+  };
+
+  # Ignore 'winborder' for popups (fixes borders in telescope.nvim)
+  # https://github.com/nvim-telescope/telescope.nvim/issues/3436
+  plenary-nvim = prev.plenary-nvim.overrideAttrs {
+    patches = fetchpatch {
+      url = "https://github.com/nvim-lua/plenary.nvim/commit/7750bc895a1f06aa7a940f5aea43671a74143be0.diff";
+      hash = "sha256-4RcuaCDc6z8pnySjLg6thyRI/FldMeyZU/WWHy0W5wU=";
     };
   };
 }
