8d84e2976fad3f73011ad0012cf3e8b3780f7052
Stanislav Asunkin <1353637+stasjok@users.noreply.github.com>
2025-09-07 17:19:27 +0300

    refactor(nvim): set stable currency column in bean-format
    This avoids changing of width when accounts are changed

--- nvim/lsp/none-ls.nix
+++ nvim/lsp/none-ls.nix
@@ -8,7 +8,11 @@
         bean_format = {
           enable = true;
           # beancount v3 requires '-' to format stdin
-          settings.args = [ "-" ];
+          settings.args = [
+            "--currency-column"
+            "78"
+            "-"
+          ];
           # Already installed by home-manager, need to override by nix-develop
           package = null;
         };
