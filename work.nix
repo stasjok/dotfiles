75a01db42daf1255ccd3d94390020797f24b61ff
Stanislav Asunkin <1353637+stasjok@users.noreply.github.com>
2025-11-05 17:09:12 +0300

    fix(work): set proxy for codecompanion

--- work.nix
+++ work.nix
@@ -1,12 +1,14 @@
+let
+  proxy = "http://rmt4prod.msk.absolutbank.ru:3128";
+in
 {
-  home.sessionVariables =
-    let
-      proxy = "http://rmt4prod.msk.absolutbank.ru:3128";
-    in
-    {
-      # Proxy settings
-      HTTP_PROXY = proxy;
-      HTTPS_PROXY = proxy;
-      NO_PROXY = "127.0.0.1,localhost,msk.absolutbank.ru";
-    };
+  home.sessionVariables = {
+    # Proxy settings
+    HTTP_PROXY = proxy;
+    HTTPS_PROXY = proxy;
+    NO_PROXY = "127.0.0.1,localhost,msk.absolutbank.ru";
+  };
+
+  # CodeCompanion proxy
+  programs.nixvim.plugins.codecompanion.settings.adapters.http.opts.proxy = proxy;
 }
