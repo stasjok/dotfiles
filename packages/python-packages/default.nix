3558ddc7d4cc9cee4faf0da0fbca2ef118f47f6c
Stanislav Asunkin <1353637+stasjok@users.noreply.github.com>
2026-01-02 18:32:32 +0300

    chore(overlay): update ansible to 2.16.15

--- packages/python-packages/default.nix
+++ packages/python-packages/default.nix
@@ -3,18 +3,26 @@ final: prev: {
   # Integrate ansible_mitogen
   ansible-core = prev.ansible-core.overridePythonAttrs (prevAttrs: rec {
     # Latest version supporting python 2.7 and 3.6
-    version = "2.16.13";
+    version = "2.16.15";
     src = fetchPypi {
       pname = "ansible_core";
       inherit version;
-      hash = "sha256-RRlOEe/jxMDJuwsRK23cHKUCigG+a0G65ZNXaosRJ68=";
+      hash = "sha256-/SmSWD0Snr2MlYXsMJ/8xhtrnhusTfnaoI+0AV8WaUs=";
     };
 
     makeWrapperArgs = [
       "--suffix ANSIBLE_STRATEGY_PLUGINS : ${final.mitogen}/${final.python.sitePackages}/ansible_mitogen"
       "--set-default ANSIBLE_STRATEGY mitogen_linear"
     ];
-    dependencies = prevAttrs.dependencies ++ [ final.mitogen ];
+
+    dependencies = lib.unique (
+      prevAttrs.dependencies
+      ++ (with final; [
+        mitogen
+        # json_query filter
+        jmespath
+      ])
+    );
   });
 
   ansible = prev.ansible.overridePythonAttrs (prevAttrs: rec {
@@ -24,13 +32,5 @@ final: prev: {
       inherit version;
       hash = "sha256-VFVzk/rldo7mQwSRxVt094Mdid0ZjT10Qx7arkQAQpg=";
     };
-
-    dependencies = lib.unique (
-      prevAttrs.dependencies
-      ++ (with final; [
-        # json_query filter
-        jmespath
-      ])
-    );
   });
 }
