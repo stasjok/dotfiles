99297345be6ff05ca2c6a0a7e540f80b8c6a8671
Stanislav Asunkin <1353637+stasjok@users.noreply.github.com>
2026-01-08 18:35:47 +0300

    feat(nvim): add basic settings for blink-cmp

--- nvim/plugins/blink-cmp.nix
+++ nvim/plugins/blink-cmp.nix
@@ -1,6 +1,12 @@
 {
   plugins.blink-cmp = {
     enable = true;
+
+    settings = {
+      keymap.preset = "enter";
+      appearance.nerd_font_variant = "normal";
+      completion.documentation.auto_show = true;
+    };
   };
 
   performance.combinePlugins.pathsToLink = [ "/target/release" ];
