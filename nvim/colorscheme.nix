8f147b41390e61f245b62c4433c5a890ad08a8b1
Stanislav Asunkin <1353637+stasjok@users.noreply.github.com>
2026-01-08 21:52:35 +0300

    refactor(nvim): remove defaults from catppuccin configuration

--- nvim/colorscheme.nix
+++ nvim/colorscheme.nix
@@ -12,36 +12,8 @@
         cmp = true;
         diffview = true;
         gitsigns = true;
-        markdown = true;
-        mini = {
-          enabled = true;
-          indentscope_color = "text";
-        };
-        native_lsp = {
-          enabled = true;
-          virtual_text = {
-            errors = [ "italic" ];
-            hints = [ "italic" ];
-            warnings = [ "italic" ];
-            information = [ "italic" ];
-            ok = [ "italic" ];
-          };
-          underlines = {
-            errors = [ "underline" ];
-            hints = [ "underline" ];
-            warnings = [ "underline" ];
-            information = [ "underline" ];
-            ok = [ "underline" ];
-          };
-          inlay_hints = {
-            background = true;
-          };
-        };
-        semantic_tokens = true;
-        telescope = {
-          enabled = true;
-        };
-        treesitter = true;
+        mini.enabled = true;
+        telescope.enabled = true;
       };
 
       custom_highlights = {
