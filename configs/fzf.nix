3a996befcd323077b2685bbfc622905f6c3c6e70
Stanislav Asunkin <1353637+stasjok@users.noreply.github.com>
2026-01-08 16:51:48 +0300

    fix(fzf): fix fzf integration in fish and increase popup size
    fzf package from nixpkgs doesn't place a script to enable it in
    vendor_conf.d anymore.

--- configs/fzf.nix
+++ configs/fzf.nix
@@ -1,13 +1,11 @@
 {
   programs.fzf = {
     enable = true;
-    # It's already configured from ~/.nix-profile/share/fish/vendor_conf.d
-    enableFishIntegration = false;
 
     # Tmux integration
     tmux = {
       enableShellIntegration = true;
-      shellIntegrationOptions = [ "-p" ];
+      shellIntegrationOptions = [ "-p80%,60%" ];
     };
   };
 }
