b34a15844c1f9d99c1abeec47c6635dc4b035c5b
Stanislav Asunkin <1353637+stasjok@users.noreply.github.com>
2026-01-13 11:21:02 +0300

    refactor(nvim): migrate my mappings from nvim-cmp to blink.cmp

--- nvim/plugins/blink-cmp.nix
+++ nvim/plugins/blink-cmp.nix
@@ -3,11 +3,43 @@
     enable = true;
 
     settings = {
-      keymap.preset = "enter";
       appearance.nerd_font_variant = "normal";
       completion = {
+        list.selection.preselect = false;
         menu.max_height = 20;
         documentation.auto_show = true;
+      };
+
+      # Mappings
+      keymap = {
+        preset = "none";
+        "<C-N>" = [
+          "select_next"
+          "fallback"
+        ];
+        "<C-P>" = [
+          "select_prev"
+          "fallback"
+        ];
+        "<Tab>" = [
+          "select_next"
+          "fallback"
+        ];
+        "<S-Tab>" = [
+          "select_prev"
+          "fallback"
+        ];
+        "<CR>" = [
+          "accept"
+          "fallback"
+        ];
+        "<C-Y>" = [
+          "accept"
+          "show"
+        ];
+        "<C-E>" = [ "hide" ];
+        "<M-d>" = [ "scroll_documentation_down" ];
+        "<M-u>" = [ "scroll_documentation_up" ];
       };
     };
   };
