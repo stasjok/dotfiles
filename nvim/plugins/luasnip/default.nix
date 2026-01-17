4642aaad9d9b0ed4264a0d68e3e44bce372f85ed
Stanislav Asunkin <1353637+stasjok@users.noreply.github.com>
2026-01-17 16:14:53 +0300

    refactor(nvim): move luasnip ft_func to nixvim options

--- nvim/plugins/luasnip/default.nix
+++ nvim/plugins/luasnip/default.nix
@@ -3,8 +3,6 @@
   plugins.luasnip = {
     enable = true;
 
-    luaConfig.pre = builtins.readFile ./pre.lua;
-
     settings = {
       updateevents = [
         "TextChanged"
@@ -12,12 +10,35 @@
       ];
       region_check_events = "InsertEnter";
       cut_selection_keys = "<C-H>";
-      snip_env = {
-        __snip_env_behaviour = "set";
-      };
-      ft_func = lib.nixvim.mkRaw "ft_func";
+
+      ft_func = lib.nixvim.mkRaw ''
+        setmetatable({
+          jinja = require("snippets.jinja_utils").jinja_ft_func("jinja"),
+          salt = require("snippets.jinja_utils").jinja_ft_func("salt"),
+          ansible = require("snippets.jinja_utils").jinja_ft_func("ansible"),
+          lua = function()
+            local buf_name = vim.api.nvim_buf_get_name(0)
+            if buf_name:sub(-9, #buf_name) == "_spec.lua" then
+              return { "lua", "lua_spec" }
+            else
+              return { "lua" }
+            end
+          end,
+        }, {
+          __call = function(tbl)
+            local filetypes = {}
+            for ft in vim.gsplit(vim.bo.filetype, ".", { plain = true }) do
+              for _, filetype in ipairs(tbl[ft] and tbl[ft]() or { ft }) do
+                table.insert(filetypes, filetype)
+              end
+            end
+            return filetypes
+          end,
+          })
+      '';
+
       load_ft_func = lib.nixvim.mkRaw ''
-        extend_load_ft({
+        require("luasnip.extras.filetype_functions").extend_load_ft({
           jinja = {
             "jinja_statements",
             "jinja_stuff",
@@ -56,6 +77,7 @@
           },
         })
       '';
+
       parser_nested_assembler = lib.nixvim.mkRaw ''
         function(pos, snip)
           local s = require("luasnip.nodes.snippet").S
@@ -67,6 +89,10 @@
           return c(pos, { i(nil, snip_text), snip })
         end
       '';
+
+      snip_env = {
+        __snip_env_behaviour = "set";
+      };
     };
 
     # Filetypes
