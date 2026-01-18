2efa9a5fdb608869d282bf3f596e5677d0e772ca
Stanislav Asunkin <1353637+stasjok@users.noreply.github.com>
2026-01-18 18:19:25 +0300

    refactor(nvim): migrate beancount snippets to options

--- nvim/snippets/beancount.nix
+++ nvim/snippets/beancount.nix
@@ -1,12 +1,12 @@
 { pkgs, ... }:
 {
-  extraFiles = {
-    # Snippets for beancount from Lencerf/vscode-beancount
-    "snippets/beancount.json" = {
+  snippets.vscode = [
+    {
+      language = "beancount";
       source = pkgs.fetchurl {
         url = "https://raw.githubusercontent.com/Lencerf/vscode-beancount/refs/tags/v0.13.0/snippets/beancount.json";
         hash = "sha256-2WZVGLERfPoQikFSbBfQdzli9rhGsh6HutP/K1HY/BI=";
       };
-    };
-  };
+    }
+  ];
 }
---
+++ nvim/snippets/default.nix
@@ -0,0 +1,7 @@
+{
+  snippets.enable = true;
+
+  imports = [
+    ./beancount.nix
+  ];
+}
