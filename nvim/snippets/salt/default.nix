556584e3e40b468c4f10a17449c606dc33cf70b0
Stanislav Asunkin <1353637+stasjok@users.noreply.github.com>
2026-01-18 21:13:31 +0300

    refactor(nvim): migrate special salt snippets to nixvim options

--- nvim/snippets/salt/default.nix
+++ nvim/snippets/salt/default.nix
@@ -2,4 +2,11 @@
   imports = [
     ./salt.nix
   ];
+
+  snippets.lua = {
+    salt_filters.text = builtins.readFile ./salt_filters.lua;
+    salt_jinja_stuff.text = builtins.readFile ./salt_jinja_stuff.lua;
+    salt_statements.text = builtins.readFile ./salt_statements.lua;
+    salt_tests.text = builtins.readFile ./salt_tests.lua;
+  };
 }
