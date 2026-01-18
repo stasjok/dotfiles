5b4310853be82f0c93ac3c9da83d1143867ba2f5
Stanislav Asunkin <1353637+stasjok@users.noreply.github.com>
2026-01-18 13:16:35 +0300

    refactor(nvim): multiple improvements to the snippets module
    Most important is automatic coercion to list of strings.

--- modules/programs/nixvim/snippets.nix
+++ modules/programs/nixvim/snippets.nix
@@ -6,60 +6,52 @@
 }:
 
 let
-  inherit (lib) mkEnableOption mkOption mkIf;
-  inherit (lib.types)
-    str
-    listOf
-    attrsOf
-    submodule
-    oneOf
-    ;
+  inherit (lib) mkOption mkEnableOption mkIf;
+  inherit (lib.types) attrsOf listOf submodule;
 
   cfg = config.snippets;
+
   jsonFormat = pkgs.formats.json { };
+  listOrStr = with lib.types; coercedTo str lib.singleton (listOf str);
 
-  # Snippet submodule type
-  vscodeSnippetType = submodule {
-    freeformType = jsonFormat.type;
-    options = {
-      prefix = mkOption {
-        type = oneOf [
-          str
-          (listOf str)
-        ];
-        description = "Snippet prefix";
+  # A submodule type for VS Code snippets
+  vscodeSnippet = submodule (
+    { config, ... }:
+    {
+      freeformType = jsonFormat.type;
+      options = {
+        prefix = mkOption {
+          description = "Snippet prefix";
+          type = listOrStr;
+        };
+        body = mkOption {
+          description = "Snippet body";
+          type = listOrStr;
+        };
+        description = mkOption {
+          description = "Snippet description";
+          type = listOrStr;
+          default = config.prefix;
+        };
       };
-      body = mkOption {
-        type = oneOf [
-          str
-          (listOf str)
-        ];
-        description = "Snippet body";
-      };
-      description = mkOption {
-        type = str;
-        default = "";
-        description = "Snippet description";
-      };
-    };
-  };
+    }
+  );
 
-  # Generate snippet files and package.json
+  # Generate VS Code snippet files and package.json
   vscodeSnippets = lib.imap1 (idx: contrib: rec {
     inherit (contrib) language;
-    path = "${builtins.head contrib.language}-${toString idx}.json";
+    path = "${toString idx}-${builtins.head contrib.language}.json";
     drv = jsonFormat.generate path contrib.snippets;
   }) cfg.vscode;
-
   packageJsonFile = jsonFormat.generate "package.json" {
     contributes = {
       snippets = map (contrib: { inherit (contrib) language path; }) vscodeSnippets;
     };
   };
-
-  snippetFiles = builtins.catAttrs "drv" vscodeSnippets;
-
-  vscodeSnippetsDrv = pkgs.linkFarmFromDrvs "vscode-snippets" ([ packageJsonFile ] ++ snippetFiles);
+  vscodeSnippetFiles = builtins.catAttrs "drv" vscodeSnippets;
+  vscodeSnippetsDrv = pkgs.linkFarmFromDrvs "vscode-snippets" (
+    [ packageJsonFile ] ++ vscodeSnippetFiles
+  );
 in
 {
   options.snippets = {
@@ -69,11 +61,11 @@ in
       type = listOf (submodule {
         options = {
           language = mkOption {
-            type = listOf str;
+            type = listOrStr;
             description = "Language(s) for these snippets";
           };
           snippets = mkOption {
-            type = attrsOf vscodeSnippetType;
+            type = attrsOf vscodeSnippet;
             default = { };
             description = "Snippets for this language";
           };
