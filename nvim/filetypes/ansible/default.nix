ffab91de911521fd2b8abd9ce0847d859241cda2
Stanislav Asunkin <1353637+stasjok@users.noreply.github.com>
2026-02-04 23:44:01 +0300

    refactor(nvim): move ansible plugin options to filetypes directory

--- nvim/filetypes/ansible/default.nix
+++ nvim/filetypes/ansible/default.nix
@@ -1,4 +1,19 @@
 {
+  # Plugin
+  plugins.ansible = {
+    enable = true;
+
+    settings = {
+      attribute_highlight = "od";
+      unindent_after_newline = 1;
+      extra_keywords_highlight = 1;
+      template_syntaxes = {
+        "*.sh.j2" = "sh";
+      };
+    };
+  };
+
+  # Ftdetect
   filetype = {
     pattern = {
       # Ansible
