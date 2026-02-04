138e08d6eb9a13f22d469ccecbbf28fcfabd6665
Stanislav Asunkin <1353637+stasjok@users.noreply.github.com>
2026-02-04 23:51:43 +0300

    refactor(nvim): replace vim-mediawiki input with fetchurl

--- flake.nix
+++ flake.nix
@@ -25,10 +25,6 @@
     };
 
     # Neovim plugins
-    vim-mediawiki = {
-      url = "github:m-pilia/vim-mediawiki";
-      flake = false;
-    };
     smart-splits-nvim = {
       # Pin smart-splits.nvim to the version that doesn't run tmux commands on startup
       url = "github:mrjones2014/smart-splits.nvim?rev=159c4823e3a11c79bb65fc4b8560320c49f738f4";
