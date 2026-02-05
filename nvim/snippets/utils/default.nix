57c98878f9dedf018327960c78458393cbe087a9
Stanislav Asunkin <1353637+stasjok@users.noreply.github.com>
2026-02-05 09:50:23 +0300

    refactor(nvim): move files relevant to snippets to snippets/utils

--- nvim/snippets/utils/default.nix
+++ nvim/snippets/utils/default.nix
@@ -8,6 +8,9 @@
         "lua/snippets/jinja_utils.lua"
         "lua/snippets/nodes.lua"
         "lua/snippets/show_conditions.lua"
+        "lua/treesitter/utils.lua"
+        "queries/jinja2/ft_func.scm"
+        "queries/yaml/ft_func.scm"
       ]
       (path: {
         text = builtins.readFile ./${path};
