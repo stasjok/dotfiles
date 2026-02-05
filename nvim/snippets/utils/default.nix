0f6873909ef19da540c17ddedf23dec412701629
Stanislav Asunkin <1353637+stasjok@users.noreply.github.com>
2026-02-05 11:37:40 +0300

    refactor(nvim): use paths instead of strings in snippet utils

--- nvim/snippets/utils/default.nix
+++ nvim/snippets/utils/default.nix
@@ -1,18 +1,21 @@
 { lib, ... }:
 {
   extraFiles =
-    lib.genAttrs
+    lib.pipe
       [
-        "lua/snippets/expand_conditions.lua"
-        "lua/snippets/functions.lua"
-        "lua/snippets/jinja_utils.lua"
-        "lua/snippets/nodes.lua"
-        "lua/snippets/show_conditions.lua"
-        "lua/treesitter/utils.lua"
-        "queries/jinja2/ft_func.scm"
-        "queries/yaml/ft_func.scm"
+        ./lua/snippets/expand_conditions.lua
+        ./lua/snippets/functions.lua
+        ./lua/snippets/jinja_utils.lua
+        ./lua/snippets/nodes.lua
+        ./lua/snippets/show_conditions.lua
+        ./lua/treesitter/utils.lua
+        ./queries/jinja2/ft_func.scm
+        ./queries/yaml/ft_func.scm
       ]
-      (path: {
-        text = builtins.readFile ./${path};
-      });
+      [
+        (map (path: lib.removePrefix (toString ./. + "/") (toString path)))
+        (lib.flip lib.genAttrs (path: {
+          text = builtins.readFile ./${path};
+        }))
+      ];
 }
