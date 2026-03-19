1fdb03a54ae54460214fd695a551221a9767a32b
Stanislav Asunkin <1353637+stasjok@users.noreply.github.com>
2026-03-19 16:58:41 +0300

    Revert "fix(nvim): fix lsp root_dir in diffview virtual files"
    This reverts commit 8affd923f2685f426664e7cb26d9c613ba135e00.
    
    Nvim was updated to 0.11.6 where the patches are already applied.

--- nvim/patches.nix
+++
@@ -1,7 +0,0 @@
-{ config, ... }:
-{
-  # Reload patched modules compiled into Nvim binary
-  extraConfigLuaPre = ''
-    vim.fs = dofile("${config.build.nvimPackage.unwrapped}/share/nvim/runtime/lua/vim/fs.lua")
-  '';
-}
