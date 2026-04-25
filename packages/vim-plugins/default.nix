79230d1abab59f241342d934531a4890b3e3a62c
Stanislav Asunkin <1353637+stasjok@users.noreply.github.com>
2026-04-25 10:52:58 +0300

    chore(overlay): update vimPlugins.codecompanion-nvim

--- packages/vim-plugins/default.nix
+++ packages/vim-plugins/default.nix
@@ -57,12 +57,12 @@ final: prev: {
 
   codecompanion-nvim = prev.codecompanion-nvim.overrideAttrs (
     finalAttrs: prevAttrs: {
-      version = "19.11.0";
+      version = "19.12.0";
       src = fetchFromGitHub {
         owner = "olimorris";
         repo = "codecompanion.nvim";
         tag = "v${finalAttrs.version}";
-        hash = "sha256-z8zcGgq5CBq5OlUZ+GfcvCgVrrFdGUMpJYR0duMigXA=";
+        hash = "sha256-SKqRMFG5xiJHhMCexOtQGAJRFYRgcfuInxTJvXhGhhI=";
       };
 
       patches = [
