2910666bf2931aec7062e6b4a8d0bf9efe7b664c
Stanislav Asunkin <1353637+stasjok@users.noreply.github.com>
2026-07-13 10:53:35 +0300

    fix(nvim): set proxy globally for codecompanion adapters

--- home.nix
+++ home.nix
@@ -1,12 +1,7 @@
-{ config, ... }:
 {
   # Nixvim
   programs.nixvim = {
-    # Proxy for Jina adapter
-    plugins.codecompanion.settings.adapters.http.jina = config.lib.nixvim.mkRaw ''
-      require("codecompanion.adapters.http").extend("jina", ${
-        config.lib.nixvim.toLuaObject { opts.proxy = "socks://server.home.stasjok.ru:13128"; }
-      })
-    '';
+    # Proxy for adapters
+    plugins.codecompanion.settings.adapters.http.opts.proxy = "socks://server.home.stasjok.ru:13128";
   };
 }
