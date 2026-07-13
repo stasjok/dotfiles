0fab8f6a92362d2cac62ce72fbe4857b24f2199e
Stanislav Asunkin <1353637+stasjok@users.noreply.github.com>
2026-07-13 13:56:11 +0300

    chore(overlay): remove otter-nvim patch

--- packages/vim-plugins/default.nix
+++ packages/vim-plugins/default.nix
@@ -42,10 +42,6 @@ final: prev: {
     };
   });
 
-  otter-nvim = prev.otter-nvim.overrideAttrs {
-    patches = ./otter-fix-user-commands.patch;
-  };
-
   # Fixes errors in telescope keymaps picker
   telescope-nvim = prev.telescope-nvim.overrideAttrs {
     patches = ./telescope-keymaps-picker.patch;
