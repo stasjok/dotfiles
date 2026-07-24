71e27a6ac3d38832751ec44d23515316d473f341
Stanislav Asunkin <1353637+stasjok@users.noreply.github.com>
2026-07-24 14:55:54 +0300

    fix(fish): erase fish_key_bindings variable after 4.3
    During migration the file in .config/fish/conf.d is created. I removed
    this file, but I'll leave its content here, because I still have many
    old shells running in tmux across devices.

--- fish/interactive.fish
+++ fish/interactive.fish
@@ -2,3 +2,12 @@
 if string match -q '*-256color' $TERM
     set --global fish_term24bit 1
 end
+
+# Prior to version 4.3, fish shipped an event handler that runs
+# `set --universal fish_key_bindings fish_default_key_bindings`
+# whenever the fish_key_bindings variable is erased.
+# This means that as long as any fish < 4.3 is still running on this system,
+# we cannot complete the migration.
+# As a workaround, erase the universal variable at every shell startup.
+# TODO: remove when all fish shells <4.3 are closed
+set --erase --universal fish_key_bindings
