b0edfbe50e79ccac366950f2490c173cb1b05da6
Stanislav Asunkin <1353637+stasjok@users.noreply.github.com>
2026-07-30 15:53:55 +0300

    fix(nvim): re-enable ansible-vim plugin
    I need a syntax for ansible_hosts and indent from there.
    Also some filetype options are useful. But I have to remove tree-sitter
    queries, because they conflict with mine.

--- nvim/filetypes/ansible/default.nix
+++ nvim/filetypes/ansible/default.nix
@@ -1,4 +1,18 @@
 { config, myLib, ... }: {
+  # Plugin
+  plugins.ansible = {
+    enable = true;
+
+    settings = {
+      attribute_highlight = "od";
+      unindent_after_newline = 1;
+      extra_keywords_highlight = 1;
+      template_syntaxes = {
+        "*.sh.j2" = "sh";
+      };
+    };
+  };
+
   # Activate yaml parser for ansible
   ftplugin.ansible = {
     content = /* lua */ ''
