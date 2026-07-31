b4c4dc074def462dc9876d9a21c05f6dc6d61111
Stanislav Asunkin <1353637+stasjok@users.noreply.github.com>
2026-07-31 16:48:31 +0300

    refactor(nvim): support tree-sitter highlight for jinja.* filetypes

--- nvim/filetypes/jinja/treesitter.lua
+++ nvim/filetypes/jinja/treesitter.lua
@@ -1,15 +1,15 @@
 local jinja_parser_path = vim.api.nvim_get_runtime_file("parser/jinja.so", false)[1]
 
 vim.api.nvim_create_autocmd("FileType", {
-  pattern = "*.jinja",
+  pattern = { "*.jinja", "jinja.*" },
   group = vim.api.nvim_create_augroup("jinja_treesitter", { clear = true }),
   callback = function(args)
     local buf = args.buf
     local filetype = args.match
-    local embedded_filetype = filetype:match("^(.+)%.jinja$")
+    local embedded_filetype = filetype:match("^(.+)%.jinja$") or filetype:match("^jinja%.(.+)$")
     local embedded_lang = embedded_filetype and vim.treesitter.language.get_lang(embedded_filetype)
     local has_embedded_parser = embedded_lang and vim.treesitter.language.add(embedded_lang)
-    local compound_lang = "jinja_compound_" .. filetype:gsub("[^%w_]", "_")
+    local compound_lang = "jinja_compound_" .. embedded_filetype:gsub("[^%w_]", "_")
     local has_jinja_parser = jinja_parser_path
       and vim.treesitter.language.add(compound_lang, {
         path = jinja_parser_path,
