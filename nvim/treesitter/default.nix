1c098f1682c7ceb176e8c5fea79103f6a0029b32
Stanislav Asunkin <1353637+stasjok@users.noreply.github.com>
2026-07-31 18:27:46 +0300

    fix(nvim): avoid double treesitter start for jinja.* filetypes

--- nvim/treesitter/default.nix
+++ nvim/treesitter/default.nix
@@ -10,7 +10,7 @@
       enable = true;
       disable = lib.nixvim.mkRaw ''
         function(_, _, ft)
-          return ft:find("%.jinja2?$") or vim.list_contains({
+          return ft:find("%.jinja2?$") or ft:find("^jinja2?%.") or vim.list_contains({
             -- Disable for filetypes for which builtin ftplugin already enables tree-sitter highlight
             "lua",
             "help",
