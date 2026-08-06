74734b5117abf1a50a9414241b754b1f228477cb
Stanislav Asunkin <1353637+stasjok@users.noreply.github.com>
2026-08-06 18:17:50 +0300

    refactor(nvim): highlight mediawiki headings like in markdown

--- nvim/filetypes/mediawiki/default.nix
+++ nvim/filetypes/mediawiki/default.nix
@@ -7,5 +7,17 @@
     wikitext = "mediawiki";
   };
 
+  # Tree-sitter queries
   extraFiles."queries/wikitext/highlights.scm".text = builtins.readFile ./queries/highlights.scm;
+
+  # Highlight headings like in markdown
+  highlight = {
+    "@markup.heading.wikitext".link = "@markup.heading.markdown";
+    "@markup.heading.1.wikitext".link = "@markup.heading.1.markdown";
+    "@markup.heading.2.wikitext".link = "@markup.heading.2.markdown";
+    "@markup.heading.3.wikitext".link = "@markup.heading.3.markdown";
+    "@markup.heading.4.wikitext".link = "@markup.heading.4.markdown";
+    "@markup.heading.5.wikitext".link = "@markup.heading.5.markdown";
+    "@markup.heading.6.wikitext".link = "@markup.heading.6.markdown";
+  };
 }
