d61b6b3f4c5d4ce082c8a6b7c25d46db42ebb150
Stanislav Asunkin <1353637+stasjok@users.noreply.github.com>
2026-09-11 11:24:26 +0300

    fix(nvim): update ft_func queries for jinja parser update

--- nvim/snippets/utils/queries/jinja/ft_func.scm
+++ nvim/snippets/utils/queries/jinja/ft_func.scm
@@ -1,7 +1,20 @@
 (render_expression
   (expression) @jinja)
 
-(control
-  (statement) @jinja)
+([
+  (statement) ; top-level node for all oneline statements
+  (autoescape_statement)
+  (block_statement)
+  (call_statement)
+  (elif_statement)
+  (filter_statement)
+  (for_statement)
+  (if_statement)
+  (macro_statement)
+  (pluralize_statement)
+  (set_block_statement)
+  (trans_statement)
+  (with_statement)
+] @jinja)
 
 (content) @text
