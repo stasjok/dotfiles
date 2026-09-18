92eea0d52754383e75504fb64e5675e8c72d023e
Stanislav Asunkin <1353637+stasjok@users.noreply.github.com>
2026-09-18 13:29:36 +0300

    refactor(nvim): remove not needed quotes

--- nvim/ai/codecompanion/default.nix
+++ nvim/ai/codecompanion/default.nix
@@ -61,7 +61,7 @@ in
               })
             end
           '';
-          "llama" = mkRaw ''
+          llama = mkRaw ''
             llamacpp_adapter("http://127.0.0.1:18081", ${
               toLuaObject {
                 opts.proxy = "";
